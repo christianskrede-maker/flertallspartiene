52efbe3c8a0b7530d046689709a96f1a05f95fe5
christianskrede-maker <christianskrede@gmail.com>
2026-05-30 20:29:54 +0200

    Update kpaInnhold.ts

--- nettsted/app/(beskyttet)/saker/data/kpaInnhold.ts
+++ nettsted/app/(beskyttet)/saker/data/kpaInnhold.ts
@@ -57,7 +57,6 @@ Bestemmelsen i 1.2 sikrer at vedtatte reguleringsplaner som Asker kommune ønske
         `,
       },
     ],
-
     gjeldendeBestemmelse: `
 1. Forholdet til vedtatte arealplaner
 
@@ -99,6 +98,144 @@ b. Reguleringsplaner for Hogstad golfbane, plan 38F, vedtatt 09.09.1992, og 38G,
 
 Opplysning:
 Med byggeformål menes arealformål etter plan- og bygningsloven (pbl.) § 12-5 nr. 1.
+    `,
+  },
+
+  "2": {
+    deler: [
+      {
+        nummer: "2.1",
+        tittel:
+          "Krav om reguleringsplan for tiltak i områder avsatt til bolig- og fritidsformål",
+        bestemmelse: `
+Jf. pbl. § 11-7 nr. 1 og 6 (pbl. § 11-9, nr. 1)
+
+I uregulerte områder avsatt til boligbebyggelse, fritidsbebyggelse eller kombinert bolig- og fritidsbebyggelse, der nødvendig teknisk infrastruktur, herunder veg, vann og avløp, ikke er opparbeidet eller sikret opparbeidet, skal det ikke gis tillatelse til etablering av ny boenhet eller opprettelse av ny tomt til bolig- eller fritidsbolig før området inngår i reguleringsplan.
+        `,
+        spesialmerknad: `
+Hensikten med bestemmelsen er å ta utgangspunkt i lovens krav, og bidra til forutsigbarhet for hva som krever regulering. Tiltak som blant annet er store i omfang, har høy kompleksitet, krever avklaringer eller som gir virkninger på for eksempel økt trafikk og hensyn til klima og natur vil reguleringsplan likevel kreves ut fra en helhetsvurdering.
+
+Tiltak som normalt ikke krever reguleringsplan:
+
+• Mindre tiltak på eksisterende boligbebyggelse i tråd med kommuneplanen, plan- og bygningslovens generelle bestemmelser og områdets karakter
+• Deling og tiltak for å sikre og videreutvikle friområdene og grønnstrukturen, og der forholdet til automatisk fredede kulturminner er avklart
+• Private/offentlig godkjente renseløsninger
+• Rivning og gjenoppbygging av bygninger som ikke er registrert som kulturminner/del av kulturmiljøer
+• Mindre tiltak innenfor idrettsanlegg knyttet til den totale bruken av idrettsanlegget
+
+Hensikten med bestemmelsen i 2.1 er å utvide den lovsatte planplikten som følger av plan- og bygningsloven § 12-1 tredje ledd.
+
+Punkt 2.1 gjelder for områder som er avsatt til bolig og fritidsbebyggelse i kommuneplanens arealdel, men som ikke er regulert. Intensjonen er at det ikke skal skje ytterligere utvikling i disse områdene før det foreligger en reguleringsplan.
+        `,
+      },
+      {
+        nummer: "2.2",
+        tittel:
+          "Krav om reguleringsplan for skogsbilveier i områder avsatt til LNFR-formål",
+        bestemmelse: `
+Jf. pbl. § 11-7 nr. 5
+
+Før det kan gis tillatelse til nyetablering eller utvidelse av skogsbilveier som utgjør terrenginngrep i skog eller skogmark på mer enn 2 dekar, eller en samlet veilengde på mer enn 2 km skal det utarbeides reguleringsplan.
+        `,
+        spesialmerknad: `
+Hensikten med bestemmelsen er å regulere etableringen av nye skogsbilveier for å redusere terrenginngrep i skog eller skogmark.
+
+Bestemmelsen fastsetter et planbestemt krav om reguleringsplan som gjelder uavhengig av om lovens plankrav gjelder.
+
+For skogsbilveier som utgjør terrenginngrep på mer enn 2 dekar eller samlet veilengde på mer enn 2 km kreves det alltid reguleringsplan før bygging kan skje.
+
+Formålet er å sikre utredning, medvirkning og politisk involvering i saker som antas å være av stor betydning for Askers befolkning, herunder frilufts-, natur- og næringsinteresser.
+        `,
+      },
+      {
+        nummer: "2.3",
+        tittel: "Opprettelse av ny grunneiendom",
+        bestemmelse: `
+Det tillates ikke opprettelse av tomt for ny bolig-, fritids-, eller næringsbebyggelse før området inngår i reguleringsplan.
+        `,
+        spesialmerknad: `
+Hensikten med bestemmelsen er å innføre plankrav for fradeling av tomt til ny bolig-, fritids-, eller næringsbebyggelse også når slik fradeling ville vært unntatt fra plankravet etter pbl. § 12-1 og bestemmelse 2.
+
+Bestemmelsen gjelder ikke arealoverføring som ikke legger til rette for nyetablering/ny utbygging.
+        `,
+      },
+    ],
+    gjeldendeBestemmelse: `
+2. Krav om reguleringsplan
+
+2.1 Krav om reguleringsplan (pbl. § 11-9, nr. 1)
+
+I områder avsatt til byggeformål, arealformål samferdselsanlegg og teknisk infrastruktur samt arealformål småbåthavn med tilhørende strandsone, kan det ikke utføres arbeid eller tiltak som nevnt i pbl. § 20-1, herunder opprettelse av ny grunneiendom og etablering av ny hovedbruksenhet, før området inngår i reguleringsplan.
+
+Opplysning:
+Med byggeformål menes arealformål etter plan- og bygningsloven (pbl.) § 11-7 nr. 1.
+
+2.2 Unntak fra krav om reguleringsplan (pbl. § 11-10, nr. 1)
+
+2.2.1 Områder avsatt til byggeformål
+
+I områder avsatt til byggeformål er følgende tiltak unntatt fra krav om reguleringsplan, forutsatt at tiltaket er i tråd med kommuneplanens bestemmelser:
+
+a. Tiltak som kan søkes uten krav til ansvarlig søker, med unntak av pbl. § 20-1, bokstav m.
+b. Tiltak unntatt søknadsplikt
+c. Tiltak i/på eksisterende bygg, herunder også tilbygg.
+d. Private/offentlig godkjente renseløsninger
+e. Riving av bygninger og anlegg, med unntak av der slike er regulert til bevaring.
+
+I uregulerte områder avsatt til boligbebyggelse utenfor 100-metersbeltet langs sjø, skal tiltak unntatt krav om reguleringsplan være i tråd med bestemmelse 14.3 Generelle boligområder.
+
+I uregulerte områder avsatt til boligbebyggelse innenfor 100-metersbeltet langs sjø, skal tiltak unntatt krav om reguleringsplan være i tråd med bestemmelse 14.5 Generelle boligområder innenfor 100-metersbeltet langs sjø.
+
+2.2.2 Områder avsatt til fritidsbebyggelse og kombinert bolig- og fritidsbebyggelse
+
+Innenfor områder avsatt til fritidsbebyggelse eller kombinert bolig- og fritidsbebyggelse (felt KBF) er tiltak som fremgår av bestemmelse 16.1 Fritidsbebyggelse og 16.2 Kombinert bebyggelse og anlegg (felt KBF), unntatt fra krav om reguleringsplan, forutsatt at tiltaket er i tråd med kommuneplanens bestemmelser.
+
+2.3 Krav om ny reguleringsplan i regulerte boligområder (pbl. § 11-9, nr. 1)
+
+2.3.1 Utbygging i strid med gjeldende regulering
+
+Ved samlet utbygging med 4 eller flere hovedbruksenheter i strid med gjeldende regulering skal det utarbeides ny reguleringsplan.
+
+2.3.2 Opprettelse av ny grunneiendom eller arealoverføring i regulert boligområde
+
+Det tillates ikke opprettelse av ny eiendom eller arealoverføring i områder regulert før 01.01.2005, uten ny reguleringsplan eller endring av gjeldende reguleringsplan.
+
+Dette gjelder også for planer vedtatt etter 01.01.2005 dersom nye tomtegrenser ikke er juridisk avklart i reguleringsplan.
+
+Krav om regulering gjelder ikke for opprettelse av ny eiendom eller arealoverføring i samsvar med tomtegrense angitt på reguleringsplankart hvor det fremgår av tegnforklaringen at angitt linje er tomtegrense.
+
+2.4 Krav om ny reguleringsplan for endring av virksomhet (pbl. § 11-9, nr. 1 og nr. 6)
+
+Såfremt forholdene nedenfor ikke er ivaretatt i gjeldende reguleringsplan, kreves ny reguleringsplan utarbeidet ved endring av virksomhet som medfører:
+
+a. Økt biltrafikk som utløser krav til utbedring av teknisk infrastruktur i tråd med kommunens til enhver tid gjeldende vei- og gatenorm.
+b. Støy over anbefalte støygrenser i tabell 2 i den til enhver tid gjeldende retningslinje for behandling av støy i arealplanlegging T-1442.
+
+2.5 Krav om ny reguleringsplan ved riving av regulert kulturminne (pbl. § 11-9, nr. 1)
+
+Riving av kulturminne som er regulert til bevaring, krever ny reguleringsplan som tillater riving.
+
+2.6 Krav om samlet plan (pbl. § 11-9, nr. 1)
+
+2.6.1 Generelt
+
+Kommunen kan kreve en samlet plan for flere eiendommer.
+
+2.6.2 Krav om samlet plan for Øvre Hallenskog
+
+For området på Øvre Hallenskog vist med bestemmelsesområde #3 på kommuneplanens arealplankart, tillates ikke opprettelse av ny grunneiendom eller ny hovedbruksenhet før det foreligger en samlet plan.
+
+2.6.3 Krav om samlet plan for Dikemarkområdet
+
+For Dikemark sentrumsområde kan tiltak som nevnt i pbl. § 20-1, herunder opprettelse av ny grunneiendom, ikke finne sted før det er utarbeidet en samlet områdeplan eller detaljreguleringsplan.
+
+2.6.4 Krav om ny samlet plan for Brønnøya
+
+Innenfor kartlagte naturtyper tillates det ikke tiltak etter pbl. § 1-6 før ny reguleringsplan for Brønnøya er vedtatt.
+
+Bruksendring av eksisterende fritidsboliger til boliger tillates ikke før ny reguleringsplan er vedtatt.
+
+Det skal utarbeides ny samlet reguleringsplan for hele Brønnøya.
     `,
   },
 } as const;
