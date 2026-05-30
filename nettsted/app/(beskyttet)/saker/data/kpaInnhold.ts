9d97f14253f76ee925d5a1f77666501387754912
christianskrede-maker <christianskrede@gmail.com>
2026-05-30 19:36:29 +0200

    Update kpaInnhold.ts

--- nettsted/app/(beskyttet)/saker/data/kpaInnhold.ts
+++ nettsted/app/(beskyttet)/saker/data/kpaInnhold.ts
@@ -1,1 +1,23 @@
+export const kpaInnhold = {
+  "1": {
+    bestemmelse: `
+1.1 Eldre arealplaner som ved kommunestyrets vedtak skal gjelde uendret.
 
+Kommunestyret vedtar hvilke eldre arealplaner som skal videreføres uendret.
+
+1.2 Eldre arealplaner som ved kommunestyrets vedtak skal oppheves.
+
+Kommunestyret vedtar hvilke eldre arealplaner som skal oppheves ved vedtak av kommuneplanens arealdel.
+    `,
+
+    spesialmerknad: `
+1.1 Bestemmelsen angir hvilke planer som fortsatt skal gjelde etter vedtak av ny kommuneplan.
+
+1.2 Bestemmelsen angir hvilke eldre planer som oppheves ved vedtak av ny kommuneplan.
+
+Planene vurderes samlet av kommunestyret som del av sluttbehandlingen av kommuneplanens arealdel.
+    `,
+
+    gjeldendeBestemmelse: "",
+  },
+} as const;
