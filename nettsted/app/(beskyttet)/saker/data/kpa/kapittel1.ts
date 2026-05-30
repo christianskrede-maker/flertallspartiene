0db8f0112ee7798527d2fb12317fc59bc8f8d813
christianskrede-maker <christianskrede@gmail.com>
2026-05-30 20:56:21 +0200

    Update kapittel1.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel1.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel1.ts
@@ -1,1 +1,102 @@
+export const kapittel1 = {
+  deler: [
+    {
+      nummer: "1.1",
+      tittel: "Kommunedelplaner som skal gjelde uendret",
+      bestemmelse: `
+Jf. Pbl. § 1-5 andre ledd, jf. § 11-15 første ledd
 
+Områder hvor vedtatte kommunedelplaner skal gjelde uendret jf. pbl. § 1-5 andre ledd, jf. § 11-15 første ledd:
+
+PlanID: 062720120244
+Plan: Kommunedelplan for Båtstø
+Vedtaksdato: 14.09.2017
+
+PlanID: 02202016014
+Plan: Kommunedelplan for E18 Slependen-Drengsrud
+Vedtaksdato: 08.11.2016
+
+Plan: Kommunedelplan for E134 strekningen Dagslett - E18 Lier
+Vedtaksdato: 18.10.2022
+
+Plan: Kommunedelplan for Holmen-Slependen
+Vedtaksdato: 29.01.2013
+      `,
+      spesialmerknad: `
+Utgangspunktet etter plan- og bygningsloven § 1-5 er at ny plan går foran eldre plan ved motstrid, med mindre noe “annet er bestemt”.
+
+Hensikten med bestemmelsen i 1.1 er å sikre at vedtatte kommunedelplaner, som Asker kommune ønsker å videreføre, ikke viker for nye bestemmelser i kommeplanens arealdel ved eventuell motstrid. Dette er hjemlet i pbl. § 1-5 andre ledd.
+      `,
+    },
+    {
+      nummer: "1.2",
+      tittel: "Reguleringsplaner som skal gjelde uendret",
+      bestemmelse: `
+Jf. pbl. § 1-5 andre ledd, jf. pbl. § 11-15 første ledd
+
+Reguleringsplaner angitt i vedlegg X skal gjelde uendret, og går foran kommuneplanen ved motstrid.
+
+PlanId:
+-
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen i 1.2 er å sikre at vedtatte reguleringsplaner som Asker kommune ønsker å videreføre, ikke viker for nye bestemmelser i kommuneplanens arealdel ved eventuell motstrid. Reguleringsplanene videreføres på denne måten uendret.
+
+Kommunen har gjort en vurdering av disse planene, og slått fast at planene fungerer godt som styringsgrunnlag fremover.
+
+Planer som videreføres uendret:
+
+Opplistingen i pkt. 1.2 tar for seg reguleringsplaner med god styringsverdi, og som angir høyere utnyttelsesgrad og høyder for ulike typer av småhusbebyggelse enn kommuneplanens arealdel, eller planer som er sammensatt av GB og SB områder eller som har en høy styringsverdi for ønsket utvikling.
+
+Planer som videføres i sin helhet, følger av vedlegg X til kommuneplanens arealdel – og vises som hensynssone i plankartet.
+
+I området hvor reguleringsplanene ikke er videreført i sin helhet etter bestemmelsen i 1.2 vil bestemmelsene i kommuneplanens arealdel gå foran bestemmelser i reguleringsplanen ved eventuell motstrid.
+
+Bestemmelsen i 1.2 sikrer at vedtatte reguleringsplaner som Asker kommune ønsker å videreføre, ikke viker for nye bestemmelser i kommuneplanens arealdel ved eventuell motstrid.
+      `,
+    },
+  ],
+
+  gjeldendeBestemmelse: `
+1. Forholdet til vedtatte arealplaner
+
+1.1 Forholdet til vedtatte kommunedelplaner (pbl. § 1-5)
+
+Følgende kommunedelplaner gjelder i Asker:
+
+• Kommunedelplan for Båtstø, vedtatt 14.09.2017
+• Kommunedelplan for E18, vedtatt 08.11.2016
+• Kommunedelplan for E134 strekningen Dagslett - E18 Lier, vedtatt 18.10.2022
+• Kommunedelplan for Holmen-Slependen, vedtatt 29.01.2013
+• Kommunedelplan for ny Røykenvei, vedtatt 03.01.1998
+
+Bestemmelser til kommunedelplan skal ved motstrid gjelde foran kommuneplan, dette gjelder ikke kommunedelplan for ny Røykenvei, strekningen Gjellum – Kloppedalen (atkomstveien til Heggedal).
+
+Arealformål i kommuneplan skal ved motstrid gjelde foran kommunedelplan, dette gjelder ikke avsatt veiformål i kommunedelplan for E18 og E134.
+
+1.2 Forholdet til vedtatte reguleringsplaner (pbl. § 1-5)
+
+Reguleringsplaner som er vedtatt før kommuneplanens arealdel, gjelder såfremt det ikke er motstrid med kommuneplanbestemmelser som i sin ordlyd overstyrer reguleringsplan.
+
+Det fremgår av den enkelte bestemmelse om den overstyrer reguleringsplan.
+
+Der reguleringsplan ikke har bestemmelse om forhold som fremgår i kommuneplan, er kommuneplanens bestemmelser utfyllende.
+
+Arealformål grønnstruktur og Landbruks-, natur og friluftsområder (LNFR) i kommuneplanens arealplankart overstyrer byggeformål i reguleringsplaners arealplankart.
+
+Ved motstrid mellom andre arealformål gjelder reguleringsplanens arealformål.
+
+Kommuneplanens bestemmelser i kapittel 20–23 for hensynssoner og båndleggingssoner overstyrer gjeldende reguleringsplaner.
+
+Avsatt veiformål med tilhørende bestemmelser i reguleringsplan for E134 går foran kommuneplan ved motstrid.
+
+Følgende planer gjelder uansett i sin helhet:
+
+a. Reguleringsplan for Vardåsen alpinanlegg, plan 23C, vedtatt 22.06.1989
+
+b. Reguleringsplaner for Hogstad golfbane, plan 38F, vedtatt 09.09.1992, og 38G, vedtatt 29.04.1993, med bestemmelser.
+
+Opplysning:
+Med byggeformål menes arealformål etter plan- og bygningsloven (pbl.) § 12-5 nr. 1.
+  `,
+} as const;
