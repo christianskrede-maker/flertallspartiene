f3ebb04abb6fa9dcade12188be10cb4118e9926d
christianskrede-maker <christianskrede@gmail.com>
2026-06-01 09:47:22 +0200

    Update route.ts

--- nettsted/app/(beskyttet)/saker/[id]/eksporter/route.ts
+++ nettsted/app/(beskyttet)/saker/[id]/eksporter/route.ts
@@ -1,1 +1,10 @@
+import { NextResponse } from "next/server";
 
+export const runtime = "nodejs";
+
+export async function GET() {
+  return NextResponse.json({
+    melding: "Samlet eksport kommer i neste versjon",
+    status: "ok",
+  });
+}
