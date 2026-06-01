9b816bd2a3153c908facd08b4dc54d4187103e04
christianskrede-maker <christianskrede@gmail.com>
2026-06-01 13:35:54 +0200

    Update arkitektur.ts

--- nettsted/lib/kpa/arkitektur.ts
+++ nettsted/lib/kpa/arkitektur.ts
@@ -1,1 +1,35 @@
+export const arkitekturKapitler = {
+  "en-ambisios-kommune": {
+    tittel: "En ambisiøs kommune",
+    ingress: "Arkitektur er samfunnsutvikling",
+    tekst: `
+Asker kommune har høye forventninger til at arkitektur og stedsutvikling brukes aktivt for å utvikle askersamfunnet...
+`,
+  },
 
+  attraktiv: {
+    tittel: "Attraktiv",
+    ingress:
+      "Helhetlig utvikling med merverdi og vakker arkitektur med særpreg.",
+    tekst: `
+Asker kommune skal ha bygg, uterom og steder som er vakre og godt utformet...
+`,
+  },
+
+  ansvarlig: {
+    tittel: "Ansvarlig",
+    ingress:
+      "Omtanke for natur, blågrønne verdier, kvalitet og varige valg.",
+    tekst: `
+Asker bruker blågrønne kvaliteter og løsninger aktivt i arkitektur og stedsforming...
+`,
+  },
+
+  "for-alle": {
+    tittel: "For alle",
+    ingress: "Gode boliger, nabolag og aktive liv i fellesskap.",
+    tekst: `
+Asker har bygg, uterom og steder som styrker livskvalitet og tilhørighet...
+`,
+  },
+};
