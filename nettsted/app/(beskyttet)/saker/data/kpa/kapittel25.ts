13981d546c568e0e2b3a22a257c6c27e875377be
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:44:49 +0200

    Update kapittel25.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel25.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel25.ts
@@ -1,3 +1,98 @@
+```ts id="m8q2vk"
 export const kapittel25 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "25",
+      tittel: "Bestemmelsesområder",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.1",
+      tittel: "Nullvekst nord",
+      bestemmelse: `Nord for nullvekstlinjen er vist med bestemmelsesområde.
+
+Det er fastsatt en nullvekstlinje på arealplankartet. Nord for nullvekstlinjen forutsettes at boligutbygging og næringslivsutvikling ikke generer vekst i personbiltrafikken. Denne målsettingen gjelder ikke sør for nullvekstlinjen.
+
+Hvorvidt et nytt byggetiltak medfører vekst i personbiltrafikken skal avklares gjennom en egen transportutredning, og en mobilitetsplan, som skal følge reguleringsplaner nord for nullvekstlinjen som tilrettelegger for ny næringsbebyggelse over 1000 m2 BRA, eller nye boligfelt med mer enn 10 hovedbruksenheter.
+
+Krav om mobilitetsplan gjelder ved søknad om etablering av, eller bruksendring til, boligformål med mer enn 10 hovedbruksenheter, samt ved etablering av eller bruksendring til næringsformål over 1000 m² BRA.`,
+      spesialmerknad: `Mobilitetsplan
+
+Mobilitetsplanen skal redegjøre for hvordan transportbehovet i planområdet er tenkt løst, med særlig vekt på løsninger som støtter opp under målsettingen om redusert personbiltrafikk. Planen skal beskrive plangrep som fremmer bruk av grønne transportformer, herunder gange, sykkel og kollektivtransport, samt bruk av deleløsninger. Det skal videre redegjøres for valgt parkeringsnorm, tilrettelegging for sykkelparkering, garderobe- og dusjfasiliteter, bruk av tjenestebiler og etablering av hensiktsmessige leveransepunkt for pakker og matvarer.
+
+Mobilitetsplanen bør også omfatte tiltak som kan iverksettes av offentlige myndigheter og kollektivselskaper for å styrke transporttilbudet til og fra området.
+
+Den trafikale effekten av foreslåtte tiltak skal dokumenteres. Relevante tiltak skal innarbeides som rekkefølgebestemmelser i reguleringsplanen, slik at de blir juridisk bindende. Ved søknad om byggetillatelse skal det dokumenteres at rekkefølgekravene er oppfylt.
+
+Parkeringsnormen legger opp til en restriktiv parkeringsnorm nord for nullvekstlinjen, og en mindre restriktiv parkeringsnorm sør for denne.`,
+    },
+    {
+      nummer: "25.2",
+      tittel: "100 m belte",
+      bestemmelse: `100-meterbeltet langs sjø er vist med bestemmelsesområde på kommuneplanens arealplankart.`,
+      spesialmerknad: `I 100-metersbeltet langs sjøen gjelder statlige planretningslinjer for differensiert forvaltning av strandsonen langs sjøen for ivaretakelse av natur- og kulturmiljø, friluftsliv, landskap og andre allmenne interesser i strandsonen unødvendig bygging langs sjøen, jf. plan- og bygningsloven (pbl.) §1-8. De statlige planretningslinjene legges til grunn i alle plan og byggesaker.`,
+    },
+    {
+      nummer: "25.3",
+      tittel: "Avvikssone støy",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.4",
+      tittel: "Rekkefølgekrav Slemmestad",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.5",
+      tittel: "Nesøya",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.6",
+      tittel: "Føyka",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.7",
+      tittel: "Ankringsområde",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.8",
+      tittel: "Prioriterte vekstområder",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.9",
+      tittel: "SB-områder",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.10",
+      tittel: "Utdaterte planer",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.11",
+      tittel: "Chemring sprengstoff fabrikk",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "25.12",
+      tittel: "GB-områder",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+  ],
 } as const;
+```
