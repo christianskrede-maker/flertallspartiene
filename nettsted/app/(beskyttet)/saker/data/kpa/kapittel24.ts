13e2faba33e45d60aed3b0d1d518265afc0ec5b0
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:43:41 +0200

    Update kapittel24.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel24.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel24.ts
@@ -1,3 +1,10 @@
 export const kapittel24 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "24",
+      tittel: "Reguleringsplaner som skal gjelde uendret",
+      bestemmelse: ``,
+      spesialmerknad: `Reguleringsplaner skal gjelde uendret`,
+    },
+  ],
 } as const;
