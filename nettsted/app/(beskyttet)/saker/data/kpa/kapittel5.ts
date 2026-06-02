2f2ac6a5a3585a140d08d6f1e948a27f6eb2e499
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:04:59 +0200

    Update kapittel5.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel5.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel5.ts
@@ -1,3 +1,240 @@
 export const kapittel5 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "5.1",
+      tittel: "Krav til andel grøntareal i utbyggingsområder",
+      bestemmelse: `I områder avsatt til utbyggingsformål, med unntak av områder avsatt til sentrumsformål, skal minst 30 prosent av netto tomteareal opparbeides som grøntareal på terreng, eller ha blågrønn faktor lik minimum 0,7 innenfor prioriterte vekstområder og min 0,8 utenfor prioriterte vekstområder.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre at byggetomter opprettholder en tilstrekkelig andel grønne og permeable arealer. Formålet er å ivareta grønne kvaliteter, overvannshåndtering, naturmangfold og gode bomiljøer. Blågrønn faktor skal dokumenteres og følges opp i plan- og byggesaker.
+
+Følgende verdier og vektingsfaktorer legges til grunn ved beregning av blågrønn faktor. Det stilles krav om minimum blågrønn faktor på 0,7 innenfor prioriterte vekstområder og minimum 0,8 utenfor prioriterte vekstområder.`,
+    },
+    {
+      nummer: "5.2",
+      tittel: "Krav til minste uteoppholdsareal, leke- og aktivitetsareal (MUA)",
+      bestemmelse: `Ved regulering eller etablering av ny boenhet skal det sikres tilstrekkelig uteoppholdsareal egnet for variert aktivitet for aktuelle alders- og brukergrupper, slik dette er angitt i tabellen nedenfor. Uteoppholdsarealene skal ta hensyn til lokalklima, og tilby både sol og skygge.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre tilstrekkelig uteoppholdsareal egnet for lek, opphold, rekreasjon og variert aktivitet for aktuelle alders- og brukergrupper. Det skal i reguleringsplanen tas stilling til hvilken møblering og hvilke leke- og aktivitetselementer som er egnet for de enkelte leke- og rekreasjonsområdene.
+
+Uteoppholdsareal, leke- og aktivitetsareal skal bestå av naturelementer og vegetasjon, ha gode lokalklimatiske forhold og gi tilgang til både sol og skygge.
+
+Innenfor sentrumsområder og prioriterte vekstområder kan krav til minste uteoppholdsareal vurderes i en helhetlig sammenheng med nærliggende offentlig tilgjengelige utearealer som tilfører området kvaliteter for lek, opphold, aktivitet og rekreasjon. En reduksjon av krav til uteoppholdsareal på egen tomt forutsetter at offentlige arealer har trygg og god tilgjengelighet, er egnet for de aktuelle brukergruppene, og er sikret og opparbeidet gjennom plan. Hvordan slike arealer inngår i den samlede vurderingen av uteoppholdsareal skal avklares og dokumenteres særskilt i reguleringsplanen.
+
+Tabellen som ligger til bestemmelsen, differensierer kravene i ulike områder. Krav til MUA med grunnlag i planens pkt. 5 kan ikke gjøres gjeldende for byggesaker etter planer som er videreført uendret jf. pkt. 1.2.`,
+    },
+    {
+      nummer: "5.2.1",
+      tittel: "Fellesbestemmelser for minste uteoppholdsareal (MUA)",
+      bestemmelse: `a.
+Areal avsatt til ferdsel, kjøring, parkering, renovasjon eller annen teknisk funksjon kan ikke medregnes i minste uteoppholdsareal.
+
+b.
+Uteoppholdsareal skal ligge på terreng og ikke være brattere enn 1:3. Ved planlegging av nye boligområder og opprettelse av ny grunneiendom for bolig skal minst 25% av uteoppholdsarealet ikke være brattere enn 1:16.
+
+c.
+Ved regulering skal minst 50% av uteoppholdsarealet som inngår i MUA, være solbelagt kl. 15.00 ved vår- og høstjevndøgn.
+
+d.
+Private uteoppholdsarealer skal sikres min 3 timers soltilgang, der dette ikke er mulig skal kompenserende tiltak sikres i utvidet fellesareal.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre at det i alle nye reguleringssaker, og i byggesaker hvor ikke tidligere reguleringsplaner er videreført uendret, blir avsatt tilstrekkelig uteoppholdsareal med nødvendige kvaliteter.`,
+    },
+    {
+      nummer: "5.2.2",
+      tittel: "Fellesbestemmelser for minste uteoppholdsareal lekeareal (MUA)",
+      bestemmelse: `Lekearealer skal utformes slik at de kan brukes av ulike aldersgrupper gjennom hele året. Arealene skal gi mulighet for variert lek, fysisk aktivitet og samhandling mellom barn, unge og voksne.
+
+Særskilte krav til minste uteoppholdsareal
+
+De 3 typene MUA er ikke overlapp-ende:
+
+MUA privat, kvm per boenhet
+
+MUA fellesareal, kvm per boenhet
+
+MUA lekeareal, *kvm per boenhet
+
+Nærleke-plass
+
+Strøksleke-plass
+
+Aktivitets-areal
+
+Generelt
+
+Privat uteoppholds-areal i direkte tilknytning til boligen.
+
+Felles uteoppholds-areal skal være egnet til opphold og aktivitet for beboere.
+
+Nærleke-plass skal ha sentral beliggenhet og maksimal gang-avstand på 50 meter fra hver boenhet i boliggate/-gruppe lekeplassen skal betjene.
+
+Strøksleke-plass skal ha sentral beliggenhet og maksimal gangavstand på 200 meter fra hver boenhet i boliggate/-gruppe lekeplassen skal betjene.
+
+Aktivitets-areal skal ha sentral beliggenhet og maksimal gangavstand på 400 meter fra hver bolig i en boliggate/-gruppe lekeplassen skal betjene.
+
+Sentrumsformål
+
+Blokker
+
+12 m2
+
+Kan være balkong, terrasse etc.
+
+Franske balkonger kan aksepteres der fellesareal økes tilsvarende.
+
+15 m2
+
+MUA felles i sentrumsområder kan omfatte offentlig arealer, med unntak av ferdselsårer (fortau, gangsykkelveier og kjørearealer). Takterrasse kan medregnes med en faktor på 0,5.
+
+15 m2 *
+
+Sosiale boformer
+
+20 m2
+
+Arealet skal ha
+
+15 m2*
+
+direkte adkomst fra innendørs fellesareal.
+
+Konsentrert småhusbebyggelse og blokk
+
+Innenfor prioritert vekstområder, med unntak av sentrumsformål
+
+Blokker
+
+12 m2
+
+Kan være balkong, terrasse etc.
+
+25 m2
+
+25 m2 *
+
+min. 250 m2
+
+Min. 1000 m2 ved mer enn 50 boenheter
+
+Min. 2000 m2 ved mer enn 150 boenheter
+
+Konsentrert småhus-bebyggelse
+
+125m2
+
+Minimum 65 m2 skal være sammen-hengende areal
+
+25 m2
+
+25 m2 *
+
+min. 250 m2
+
+min. 250 m2
+
+min. 250 m2
+
+Utenfor prioritert vekstområde
+
+Blokker
+
+12 m2
+
+Kan være balkong, terrasse etc.
+
+55 m2
+
+25 m2 *
+
+min. 250 m2
+
+min. 250 m2
+
+min. 250 m2
+
+Konsentrert småhus-bebyggelse
+
+150m2
+
+Minimum 75 m2 skal være sammenhengende areal
+
+55 m2
+
+25 m2 *
+
+min. 250 m2
+
+min. 250 m2
+
+min. 250 m2
+
+Frittliggende småhusbebyggelse
+
+Takterrasser kan medregnes i uteoppholds-areal dersom takterrassen grenser mot annet målbart uteoppholds-areal på terreng, og hvor det ikke er nivåforskjell på mer enn 20 cm mellom terrenget og terrassen.
+
+Krav til nærleke-plass gjelder ved plan-legging av 4 eller flere boenheter.
+
+Krav til strøksleke-plass gjelder ved planlegging av 20 eller flere boenheter.
+
+Krav til aktivitets-areal gjelder ved planlegging av 50 eller flere boenheter.
+
+Enebolig
+
+200 m2
+
+Minimum 75 m2 skal være sammen-hengende areal og med minste bredde på 4 meter.
+
+25 m2 *
+
+min. 250 m2 ved færre enn 10 boenheter
+
+Min. 1000 m2
+
+Min. 2000 m2
+
+Tomannsbolig
+
+150 m2 per hovedbruksenhet
+
+Minimum 75 m2 skal være sammen-hengende areal og med minste bredde på 3 meter.
+
+25 m2 *
+
+min. 250 m2 ved færre enn 10 boenheter
+
+Min. 1000 m2
+
+Min. 2000 m2
+
+Sekundær boenhet
+
+12 m2
+
+Sammen-hengende og i direkte tilknytning til boenheten.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre at det i alle nye reguleringssaker, og i byggesaker hvor ikke tidligere reguleringsplaner er videreført uendret, blir avsatt tilstrekkelig uteoppholdsareal til lek, med nødvendige kvaliteter.
+
+Tabellen som ligger til bestemmelsen, differensierer kravene i ulike områder. Nærlekeplasser skal ha kort avstand til, og være synlige fra, boligene og være tilrettelagt for de minste barna. Strøkslekeplasser skal være tilrettelagt for større barn i skolealder, i tillegg til mindre barn og opphold for voksne. Disse dekker behovet i et større område enn nærlekeplassene, og kan reguleres til nærmiljøanlegg dersom det er å anse som et slikt. Aktivitetsareal skal dekke behovene i et større område enn strøkslekeplasser, og for både små og store barn, og voksne. Aktivitetsarealer kan samlokaliseres med skoler, idrettsanlegg, friområder eller lignende. Ved regulering bør aktivitetsareal reguleres som arealformål nærmiljøanlegg (Idrett).
+
+Grunnprinsipper for utformingen av lekeareal (MUA):
+
+Hovedmålgruppen skal være styrende for hvilke funksjoner som gis prioritet ved valg av utstyr, innredning og løsninger. Utforming skal stimulere allsidig lek, utforskning og motorisk utvikling, og oppmuntre til fysisk aktivitet året rundt.
+
+Det skal være sitteplasser for voksne som en del av lekearealene.
+
+Utearealer til aking og skilek må ha god belysning og tilstrekkelig sikkerhetstiltak inkludert nok areal for oppbremsing uten fare for kollisjoner ved veganlegg, trær eller andre konstruksjoner.
+
+Belegget som velges for leke- og aktivitetsarealer bør være variert og primært bestå av naturlige materialer.
+
+Naturlig og variert terreng og vegetasjon i lekearealene kan redusere behovet for kunstige lekeinstallasjoner, så lenge funksjonskravene oppfylles. Deler av lekearealet skal være beplantet med busker og trær. Der hvor eksisterende vegetasjon ble fjernet, må det etableres ny, og stedegen vegetasjon skal benyttes.
+
+Lekeplasser skal være tilgjengelig for alle og deler av arealet skal være universelt utformet. Det skal være mulig for barn med funksjonsnedsettelse å komme bort til (noen) lekeapparater og leke sammen med andre barn.`,
+    },
+    {
+      nummer: "5.3",
+      tittel: "Parkeringsnorm",
+      bestemmelse: `Ved regulering eller søknad om tiltak skal det avsettes areal til parkering i samsvar med parkeringsnorm fastsatt i kommunens parkeringsstrategi jf. vedlegg x`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre at planinitiativ/planforslag forholder seg til kommunens parkeringsnorm. Dette skaper forutsigbarhet for utbyggere og gjør det mulig for kommunen og likebehandle forslagsstillere. Når det kommer til søknad om tiltak gjør bestemmelsen det mulig å rette opp i mangler ved gamle reguleringsplaner, og gi generelle bestemmelser som garasje og biloppstillingsplasser i reguleringsplaner som mangler slike bestemmelser. I planer som er videreført uendret vil denne bestemmelsen ikke gjelde.
+
+Innenfor senterstruktur og sentrumsformål legges det til grunn en restriktiv parkeringsnorm for beboere og arbeidsplasser, kombinert med en mindre restriktiv parkeringsnorm for handlende og brukere av service- og kulturtilbud. Dette skal bidra til å stimulere til økt handel og aktivitet i tettstedene, samtidig som bilbruk begrenses der det er alternative transportmuligheter.
+
+For blokkbebyggelse bør parkering planlegges samlet i parkeringsanlegg under bakken, med adkomst fra randsonen av boligområdet. Boligområdene skal i størst mulig grad være bilfrie, men samtidig tilgjengelige for nyttetransport og utrykningskjøretøy. Parkeringsanlegg for bil og sykkel skal lokaliseres slik at det er enkelt å velge gange, sykkel og kollektivtransport i hverdagen.`,
+    },
+  ],
 } as const;
