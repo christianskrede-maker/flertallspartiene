ffc20e5b7582ff36a94c4319001eb7acbf5ca2d4
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:18:52 +0200

    Update kapittel20.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel20.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel20.ts
@@ -1,3 +1,174 @@
+```ts id="z7n4qm"
 export const kapittel20 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "20.1",
+      tittel: "Sikringssone, nedslagsfelt drikkevann, H110",
+      bestemmelse: ``,
+      spesialmerknad: `Hensikten med hensynssone H110 er å sikre drikkevannskilder og tilhørende nedslagsfelt mot forurensning, avrenning, erosjon, utslipp og annen arealbruk eller virksomhet som kan redusere drikkevannskvaliteten eller vannforsyningens sikkerhet.
+
+Bestemmelsene gjelder for tiltak og virksomhet innenfor hensynssonen, jf. pbl. § 1-6.
+
+Det er følgende drikkevannskilder i Asker kommune: Setervannet, Sandungen, Striglevann og Husebyvannet. Bjørvann er krisevannkilde. Drikkevannskildene er gitt arealformål drikkevann i arealplankartet, og nedslagsfeltet for de aktuelle vannene er merket med hensynssone nedslagsfelt drikkevann.
+
+Før tillatelse til eventuelle tiltak på eksisterende lovlig etablerte bygninger og anlegg gis skal:
+
+det foreligge en vurdering av tiltakets betydning for drikkevannskilden. Nødvendige sikringstiltak skal være fastsatt før tillatelse gis.
+
+saken forelegges vannverkseier.
+
+Tillatelse kan ikke gis dersom vannverkseier vurderer at tiltaket kan redusere drikkevannskvaliteten eller svekke vannforsyningens sikkerhet.
+
+Mattilsynet skal orienteres om søknad og tillatelse til tiltak innenfor hensynssonen.`,
+    },
+    {
+      nummer: "20.1.1",
+      tittel: "Forbud mot tiltak",
+      bestemmelse: `Innenfor sikringssonen tillates ikke tiltak etter pbl. § 1-6 første ledd, unntatt tiltak på eksisterende bygninger.
+
+Forbudet i første ledd, første setning, gjelder ikke landbrukstiltak. Landbrukstiltak og landbruksvirksomhet innenfor hensynssonen skal ikke medføre fare for forurensning eller forringelse av drikkevannskilden.
+
+Tiltak på eksisterende lovlig etablerte bygninger og anlegg tillates når tiltaket ikke medfører:
+
+a.
+ny eller økt fare for forurensning
+
+b.
+økt belastning på avløpsløsning
+
+c.
+vesentlig terrenginngrep eller endret avrenning
+
+d.
+endret bruk som kan være uheldig for drikkevannskilden
+
+e.
+økt risiko for skade på vannforsyningen
+
+Forbudet er ikke til hinder for nødvendig drift, tilsyn, vedlikehold og sikring av drikkevannsanlegg, eller for tiltak som er uttrykkelig tillatt etter gjeldende klausuleringsbestemmelser eller særskilt tillatelse etter relevant sektorregelverk, forutsatt at tiltaket ikke kan redusere drikkevannskvaliteten.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "20.1.2",
+      tittel: "Forbud mot ferdsel og aktiviteter ved drikkevannskildene",
+      bestemmelse: `Innenfor sikringssonen er det forbudt med all ferdsel og aktiviteter som kan medføre at drikkevannet blir forurenset.`,
+      spesialmerknad: `Innenfor hensynssone H110 er det forbud mot:
+
+all ferdsel på og i vannet/drikkevannskilden, herunder også bading, vading, skøyter, båt, kajakk etc. (Forbudet gjelder også husdyr.)
+
+fisking, pilking etc.
+
+slå leir/raste (telt, hengekøye, brenne bål, campingvogn mv.) nærmere vannkanten enn 50 meter.
+
+etterlate seg søppel/avfall
+
+riding nærmere vannkanten enn 100 meter
+
+Forbudet gjelder hele året, også når vannet er islagt. Enkelte unntak kan gjelde der det er gitt særskilt tillatelse, herunder bruk av båt i forbindelse med oppsyn av drikkevannskilden.
+
+Forbudet gjelder for arealformålene LNFR, LNFR-spredt bebyggelse og vern av sjø og vassdrag(drikkevann).`,
+    },
+    {
+      nummer: "20.2",
+      tittel: "Sikringssone tiltak under terrengnivå, H190_1",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "20.2.1",
+      tittel: "Veitunneler",
+      bestemmelse: `Innenfor sikringssone for tiltak under terrengnivå, H190, gjelder bestemmelser i gjeldende kommunedelplan i sin helhet (E18 og E134).`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "20.2.2",
+      tittel: "Jernbanetunnel",
+      bestemmelse: `Innenfor sikringssone Jernbanetunnel, H190, er boring i grunnen ikke tillatt.`,
+      spesialmerknad: `Hensikten med hensynssonen er å hindre tiltak som kommer i konflikt med jernbanetunnel.
+
+Ved en eventuell dispensasjonssøknad skal kommunen vurdere om tiltaket oppfyller vilkårene i pbl. § 19-2. I den forbindelse skal saken forelegges Bane Nor for uttalelse.`,
+    },
+    {
+      nummer: "20.3",
+      tittel: "Faresone høyspentanlegg (inkludert høyspentkabler), H370",
+      bestemmelse: `Innenfor områder angitt som faresone høyspentanlegg (inkludert høyspentkabler) tillates ikke ny bebyggelse og anlegg.`,
+      spesialmerknad: `Hensikten med hensynssonen er å hindre ny bebyggelse og anlegg innenfor hensynssone høyspentanlegg for å ivareta eksisterende infrastruktur og redusere risikoen for uønskede hendelser.
+
+Anleggsarbeid og tiltak i terrenget innenfor hensynssonen skal på forhånd avklares med ledningseier.
+
+Høyspenningsanlegget kan generere elektromagnetiske felt utover hensynssonen. Netteier må kontaktes ved tiltak nær hensynssonen for å få opplysninger om spenningsnivå, byggeforbuds- og ryddebelte eller om andre elektriske installasjoner. Jordkabler er ikke omfattet av hensynssonen og må hensyntas ved tiltak. Statnett ber om at følgende reguleringsbestemmelse knyttes opp mot hensynssonen for transmisjonsnettanleggene:
+
+Det er ikke tillatt med ny bebyggelse innenfor hensynssonen. Alt anleggsarbeid og alle tiltak i terrenget innenfor hensynssonen skal på forhånd avklares med ledningseier. Det må ikke iverksettes tiltak som medfører forringelse av adkomst til Statnetts anlegg. Det må heller ikke gjøres inngripen i terrenget som medfører endring av overdekningen over jordkabler, skade på mastejording eller oppfylling av terrenget som medfører redusert høyde opp til luftledningsanlegg.
+
+Arbeid nært spenningssatt anlegg, for eksempel sprengningsarbeid, anleggsarbeid og skogsarbeid, må skje på en måte som ikke gir fare for skade på personell eller Statnetts ledninger, maskiner og utstyr.
+
+Det er en varslingsplikt for slikt arbeid dersom det skal foregå nærmere enn 30 meter målt horisontalt fra nærmeste strømførende line. Statnett skal da kontaktes seinest 6 uker før planlagt oppstart av arbeidet, slik at befaring kan gjennomføres og avtale om aktuelle sikkerhetstiltak kan inngås.
+
+Hensynssonen bør være identisk med ledningsanleggets byggeforbudsbelte. Byggeforbudsbeltet tilhørende transmisjonsnettledningene Sylling –Tegnebyog Flesaker –Tegnebyer totalt 40meter bredt for hver av ledningene, det vil si 20meter målt horisontalt til hver side fra senter for hver av ledningene.
+
+Hensynssonen for elektriske anlegg trekkes over Statnett sine tomter for muffeanlegg.
+
+Vi viser også til NVEs veileder 2/2017 - Nasjonale og vesentlige regionale interesser innen NVEs saksområder i arealplanlegging som beskriver hvordan energianlegg bør ivaretas i planen, slik at en unngår innsigelse.`,
+    },
+    {
+      nummer: "20.4",
+      tittel: "Faresone brann- og eksplosjon, H350",
+      bestemmelse: ``,
+      spesialmerknad: `Hensikten med hensynssonene er å regulere tiltak innenfor hensynssone brann- og eksplosjon for å redusere risikoen for uønskede hendelser.
+
+Hensynssonene er knyttet til eksisterende næringsvirksomheter med risiko for storulykker.`,
+    },
+    {
+      nummer: "20.4.1",
+      tittel: "Bjønndalen, H350_1 – 3",
+      bestemmelse: `Innenfor hensynsonen tillates tiltak som ikke medfører økt risiko, med unntak av offentlige bygg og institusjoner, forsamlingslokaler o.l. Der tiltaket medfører økt risiko gjelder følgende:
+
+a.
+Innenfor faresone H350_1 tillates ikke nye boenheter, bruksendring til bolig, ny bebyggelse eller anlegg med unntak av tilbygg/påbygg, (pbl. § 11-7 nr. 1) eller offentlig samferdselsanlegg (pbl. § 11-7 nr. 2).
+
+b.
+Innenfor faresone H350_2 tillates ikke nye boenheter, bruksendring til bolig, ny bebyggelse eller anlegg med unntak av tilbygg/påbygg, (pbl. § 11-7 nr. 1).
+
+c.
+Innenfor faresone H350_3 tillates ikke nye bygg med mer enn 2-3 etasjer, ny bebyggelse for offentlig eller privat tjenesteyting, (pbl. § 11-7 nr. 1) eller annen bebyggelse der flere mennesker oppholder seg i større forsamlinger.
+
+d.
+Før det gis tillatelse til tiltak etter pbl. § 1-6 innenfor hensynssonene, skal det foreligge positiv uttalelse fra Orica Norway AS og Direktoratet for samfunnssikkerhet og beredskap (DSB)
+
+e.
+Sammen med søknad om tiltak som innebærer permanent opphold av personer innenfor sonene, skal det foreligge en fagkyndig vurdering av hvilken påvirkning det farlige anlegget kan ha for tiltaket. Tillatelse kan ikke gis med mindre tiltaket kan sikres på en tilfredsstillende måte, herunder gjennom eventuelle avbøtende tiltak.`,
+      spesialmerknad: `Hensikten med hensynssonen er å regulere hindre nye tiltak som medfører økt risiko. Før det gis tillatelse til tiltak etter pbl. § 1-6 innenfor hensynssonene, skal det foreligge positiv uttalelse fra Orica Norway AS og Direktoratet for samfunnssikkerhet og beredskap (DSB)
+
+Sammen med søknad om tiltak som innebærer permanent opphold av personer innenfor sonene, skal det foreligge en fagkyndig vurdering av hvilken påvirkning det farlige anlegget kan ha for tiltaket. Tillatelse kan ikke gis med mindre tiltaket kan sikres på en tilfredsstillende måte, herunder gjennom eventuelle avbøtende tiltak.`,
+    },
+    {
+      nummer: "20.4.2",
+      tittel: "Engene, H350_4 – 5",
+      bestemmelse: `Engene, H350_4 – 5
+
+a. Innenfor hensynsone H350_4 (5 kPa) tillates ikke nye tiltak før området inngår i reguleringsplan.
+
+b. Innenfor hensynssone H350_5 (2 kPa) tillates tiltak etter pbl. § 20-1 på tomter med eksisterende bebyggelse. Oppføring av ny bebyggelse i form av ene- og tomannsboliger tillates. Annen type bebyggelse kan ikke tillates før det er fremlagt dokumentasjon på at planlagt bebyggelse ikke utløser vesentlig økt risiko.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "20.5",
+      tittel: "Faresone skytebane H360",
+      bestemmelse: `Ved bruk av skytebane er fareområdet ikke åpent for allmenn ferdsel.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "20.6",
+      tittel: "Faresone flom, H320",
+      bestemmelse: `Ved arealplanlegging og søknad om tiltak innenfor området angitt som faresone flom (H320) skal det foretas og redegjøres for nødvendige sikringstiltak.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "20.7",
+      tittel: "Faresone ras og skred (kvikkleire), H310",
+      bestemmelse: `Ved arealplanlegging og søknad om tiltak innenfor område angitt som faresone ras og skred (kvikkleire) (H310) skal det foretas og redegjøres for nødvendige sikringstiltak.`,
+      spesialmerknad: ``,
+    },
+  ],
 } as const;
+```
