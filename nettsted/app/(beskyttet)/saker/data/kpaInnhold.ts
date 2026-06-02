babcc3318daee94cc6da61a70740b88158a4e7d4
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 16:49:24 +0200

    Update kpaInnhold.ts

--- nettsted/app/(beskyttet)/saker/data/kpaInnhold.ts
+++ nettsted/app/(beskyttet)/saker/data/kpaInnhold.ts
@@ -1,9 +1,11 @@
 import { kapittel1 } from "./kpa/kapittel1";
 import { kapittel2 } from "./kpa/kapittel2";
 import { kapittel3 } from "./kpa/kapittel3";
+import { kapittel4 } from "./kpa/kapittel4";
 
 export const kpaInnhold = {
   "1": kapittel1,
   "2": kapittel2,
   "3": kapittel3,
+  "4": kapittel4,
 } as const;
