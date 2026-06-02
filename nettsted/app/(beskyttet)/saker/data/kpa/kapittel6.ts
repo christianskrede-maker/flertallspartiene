89ad098eade600683b734d0b4e3e9f7e4b1b7571
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 19:30:17 +0200

    Update kapittel6.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel6.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel6.ts
@@ -1,3 +1,116 @@
 export const kapittel6 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "6.1",
+      tittel: "Byggegrense mot offentlig vei",
+      bestemmelse: `
+a.
+Byggegrense mot offentlig vei fremgår av kommuneplanens plankart eller reguleringsplans plankart.
+
+b.
+Der byggegrense ikke fremgår av kommuneplanens plankart eller reguleringsplan, skal avstand fra bygning til regulert veiareal være minimum 5 meter i boligområder og minimum 15 meter utenfor sentrumsformål.
+
+c.
+Mindre frittliggende tiltak unntatt søknadsplikt etter pbl. § 20-5 bokstav a, herunder garasje, carport og bod, kan plasseres nærmere offentlig vei enn fastsatt byggegrense dersom tiltaket:
+•
+ikke er til hinder for trafikksikkerhet, frisikt eller fremkommelighet,
+•
+ikke vanskeliggjør drift og vedlikehold av offentlig vei, herunder snøopplag og renovasjon,
+•
+er tilpasset terreng og eksisterende bebyggelsesstruktur, og
+•
+etter kommunens vurdering har en forsvarlig plassering, jf. pbl. § 29-4.
+
+d.
+Garasje med innkjøring vinkelrett på offentlig vei skal ha minimum 5 meter oppstillingsplass mellom garasjeport og regulert veiareal.
+      `,
+      spesialmerknad: `
+Ved regulering:
+
+Mot kommunal vei skal byggegrense være minimum 4 meter fra regulert eller eksisterende veiformål.
+
+Mot fylkesvei skal byggegrense utenfor sentrumsformål være minimum 15 meter fra regulert eller eksisterende veiformål.
+      `,
+    },
+    {
+      nummer: "6.2",
+      tittel: "Byggegrense mot sjø",
+      bestemmelse: `
+pbl. §11-9 nr 5 jf. § 1-8 tredje ledd
+
+Nærmere sjø enn angitt byggegrense tillates ikke tiltak etter pbl. § 1-6, med unntak av:
+
+a.
+Tilrettelegging av offentlige; friområder, parker, kyststi, turstier, sanitæranlegg og HC-parkering, til allmenn ferdsel og bruk.
+
+b.
+Påfylling av stedegen sandtype på eksisterende offentlig sandstrand.
+
+c.
+Regulert småbåthavn i tråd med bestemmelse 9.X
+
+d.
+Regulert brygge i tråd med bestemmelse 9.X
+
+e.
+Tiltak i forbindelse med godkjent brygge etter vilkår i 9.X
+
+f.
+Nyetablering, endring, flytting og fjerning av offentlige anlegg til navigasjonsmessig bruk.
+
+g.
+Offentlig godkjente vann- og avløpsanlegg, forutsatt at disse ikke medfører søknadspliktige terrengendringer.
+
+h.
+Tiltak i/på eksisterende bebyggelse som ikke medfører økt BYA/BRA eller legger til rette for en endring eller utvidelse av bruken av bygget.
+
+i.
+Tilbygg på inntil 8m2 for sanitæranlegg når følgende vilkår er oppfylt:
+
+1.
+Tilbygget er nødvendig for å sikre avkloakkering av eiendommen ved tilkobling til godkjent vann- og avløpsanlegg, og
+
+2.
+det kan dokumenters at sanitæranlegg ikke kan integreres i eksisterende bebyggelse
+
+3.
+tilbygget plasseres i tilknytning til hovedbruksenhet, i retning bort fra sjø
+
+Nærmere sjø enn angitt byggegrense er det ikke tillat å fjerne vegetasjon, utenom normal skjøtsel, jf. pbl. § 1-6 første ledd andre punktum.
+
+Etablering av kunstige sandstrender eller påfylling av ikke-stedegen sandtype, på land eller i sjø, er ikke tillatt.
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen er å forhindre nedbygging og privatisering av strandsonen samtidig som kommunen åpner for tiltak som er samfunnsnyttige eller som i liten grad medfører nedbygging eller privatisering.
+
+Plan og bygningslovens byggeforbud i strandsonen (pbl. § 1-8) gjelder ikke der det er satt annen byggegrense i plan. Kommuneplanen har byggegrense langs hele kysten som fremgår av arealplankartet. Nærmere sjø enn byggegrensen vil det kun være mulig å gjøre tiltak som er omfattet av unntakene i 6.2 bokstav a til i. Forbudet og unntakene i 6.2 gjelder både søknadspliktige og ikke søknadspliktige tiltak.
+
+Hensikten bak unntaket i punkt a. er at det skal være mulig å tilrettelegge arealer i strandsonen som er til allmenn ferdsel og bruk. Unntaket vil ikke omfatte for eksempel tiltak på privat eiendom som ikke er tilknyttet kyststi eller tursti som brukes av allmennheten.
+
+Hensikten bak unntaket i punkt b. er at det skal være mulig med påfyll av sand på offentlig sandstrand. Sand skylles ut i sjøen, og det vil med jevne mellomrom være behov for etterfylling for å opprettholde kvaliteten og bruken av området. Unntaket gjelder eksisterende sandstrender som er i offentlig eie og benyttes av allmenheten. Påfyllingen skal kun skje på land, og sanden skal komme fra kjente og kvalitetssikrede kilder. Omfanget av etterfylling skal begrenses til det som anses nødvendig for vedlikehold av stranden.
+
+Hensikten bak unntakene i punkt c. og d. er å ikke forby brygge eller småbåthavn som er regulert og oppfyller vilkårene i bestemmelse xx
+
+Hensikten bak unntaket i punkt e. er å muliggjøre tiltak på tidligere godkjent brygge, forutsatt at vilkårene i xxer oppfylt.
+
+Hensikten bak unntaket i punkt f. er å legge til rette for etablering og vedlikehold av nødvendige navigasjonsinnretninger.
+
+Hensikten bak unntaket i punkt g. er å muliggjøre etablering av offentlige vann og avløpsanlegg også i strandsonen. Vann- og avløpsanlegget kan være søknadspliktig, men det forutsettes at det ikke må gjøres vesentlige terrengendringer i forbindelse med tiltaket.
+
+Hensikten bak unntaket i punkt h. er å åpne for tiltak i og på eksisterende bygninger i strandsonen som ellers ville være omfattet av byggeforbudet. Unntaket omfatter både søknadspliktige og ikke søknadspliktige tiltak. Det forutsettes at tiltaket skjer i eller på eksisterende bygg, og at tiltaket ikke medfører en endring eller utvidelse av bruken av bygget. Det vil f.eks. være mulig å bruksendre fra bod (tilleggsdel) til soverom (hoveddel) i en eksisterende hytte, men ikke å bruksendre uthus/utebod til soveanneks. Installering av bad eller kjøkken vil lett kunne medføre en utvidet bruk, og vil i mange tilfeller ikke falle inn under unntaket. Rehabilitering og utskifting av eksisterende skorstein og piperør på lovlig oppført bebyggelse tillates dersom tiltaket ikke medfører økt volum eller endret plassering.
+
+Hensikten bak unntaket i punkt i. er å gjøre det mulig å oppføre tilbygg i strandsonen der det er nødvendig for å få inn sanitæranlegg i eksisterende bygg. Alle vilkårene punkt 1 til 4 må være oppfylt.
+
+Bestemmelsen er ment som en snever unntaksbestemmelse for å kunne etablere nødvendige sanitæranlegg i eksisterende boligbebyggelse hvor dette ikke lar seg løse innenfor eksisterende bygningsmasse. Vurderingen av nødvendighet må foretas konkret i den enkelte sak.
+
+Det er søker/tiltakshaver som må fremme en begrunnet søknad og redegjøre for hvorfor det er nødvendig med den prosjekterte størrelsen på tilbygget. At bestemmelsen åpner for tilbygg på inntil 8 m² innebærer ikke at det automatisk anses nødvendig med et tilbygg av denne størrelsen. Tiltaket skal begrenses til det arealet som er nødvendig for å etablere sanitæranlegget på en funksjonell og forsvarlig måte.
+
+Tilbygget skal kun benyttes til sanitærfunksjoner knyttet til bad/wc. Det tillates ikke at arealet benyttes til gangareal, oppholdsrom, bod eller andre formål utover det som er nødvendig for sanitæranlegget.
+
+Spesialmerknad til sandstrender
+
+Med stedegen sandtype menes type sand som man naturlig ville kunne finne i området. Dette må ikke forveksles med sand som tidligere er tilført området. At det tidligere er fylt på med for eksempel skjellsand, medfører ikke at man kan fylle på med skjellsand på ny. Det må gjøres en vurdering av hva som er naturlig sandtype for området.
+      `,
+    },
+  ],
 } as const;
