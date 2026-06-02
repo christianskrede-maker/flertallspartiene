13c592af6a962ac017294d5518d35ad741d7ce68
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 16:53:30 +0200

    Update kpaInnhold.ts

--- nettsted/app/(beskyttet)/saker/data/kpaInnhold.ts
+++ nettsted/app/(beskyttet)/saker/data/kpaInnhold.ts
@@ -2,10 +2,52 @@ import { kapittel1 } from "./kpa/kapittel1";
 import { kapittel2 } from "./kpa/kapittel2";
 import { kapittel3 } from "./kpa/kapittel3";
 import { kapittel4 } from "./kpa/kapittel4";
+import { kapittel5 } from "./kpa/kapittel5";
+import { kapittel6 } from "./kpa/kapittel6";
+import { kapittel7 } from "./kpa/kapittel7";
+import { kapittel8 } from "./kpa/kapittel8";
+import { kapittel9 } from "./kpa/kapittel9";
+import { kapittel10 } from "./kpa/kapittel10";
+import { kapittel11 } from "./kpa/kapittel11";
+import { kapittel12 } from "./kpa/kapittel12";
+import { kapittel13 } from "./kpa/kapittel13";
+import { kapittel14 } from "./kpa/kapittel14";
+import { kapittel15 } from "./kpa/kapittel15";
+import { kapittel16 } from "./kpa/kapittel16";
+import { kapittel17 } from "./kpa/kapittel17";
+import { kapittel18 } from "./kpa/kapittel18";
+import { kapittel19 } from "./kpa/kapittel19";
+import { kapittel20 } from "./kpa/kapittel20";
+import { kapittel21 } from "./kpa/kapittel21";
+import { kapittel22 } from "./kpa/kapittel22";
+import { kapittel23 } from "./kpa/kapittel23";
+import { kapittel24 } from "./kpa/kapittel24";
+import { kapittel25 } from "./kpa/kapittel25";
 
 export const kpaInnhold = {
   "1": kapittel1,
   "2": kapittel2,
   "3": kapittel3,
   "4": kapittel4,
+  "5": kapittel5,
+  "6": kapittel6,
+  "7": kapittel7,
+  "8": kapittel8,
+  "9": kapittel9,
+  "10": kapittel10,
+  "11": kapittel11,
+  "12": kapittel12,
+  "13": kapittel13,
+  "14": kapittel14,
+  "15": kapittel15,
+  "16": kapittel16,
+  "17": kapittel17,
+  "18": kapittel18,
+  "19": kapittel19,
+  "20": kapittel20,
+  "21": kapittel21,
+  "22": kapittel22,
+  "23": kapittel23,
+  "24": kapittel24,
+  "25": kapittel25,
 } as const;
