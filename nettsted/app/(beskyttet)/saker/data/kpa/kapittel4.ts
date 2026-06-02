c378d00880f45291efbcd6c7cb39553e2f4bd1e4
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 16:45:45 +0200

    Update kapittel4.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel4.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel4.ts
@@ -1,1 +1,40 @@
+export const kapittel4 = {
+  deler: [
+    {
+      nummer: "4.1",
+      tittel: "Tilknytning til vann- og avløpsanlegg for fritidsbebyggelse",
+      bestemmelse: `
+pbl. § 11-9, nr. 3 jf. § 30-6
 
+Plan- og bygningsloven § 27-1 andre til fjerde ledd og § 27-2 andre til fjerde ledd gjelder tilsvarende for fritidsbebyggelse jf. pbl. § 30-6.
+      `,
+      spesialmerknad: `
+Hensikten med denne bestemmelsen er å sikre at kravene i §§ 27-1 og 27-2 også gjøres gjeldende for fritidsbebyggelse og er viktig for å sikre gode og forsvarlige løsninger også for fritidsbebyggelse.
+      `,
+    },
+    {
+      nummer: "4.2",
+      tittel: "Krav til tekniske løsninger",
+      bestemmelse: `
+Jf. pbl. § 11-9
+
+• For områder omfattet av reguleringsplan vedtatt før 01.07.2009 skal kommunens vei- og gatenorm, renovasjonstekniske norm og VA-norm legges til grunn ved planlegging og gjennomføring av tiltak. For reguleringsplaner vedtatt fra og med 01.07.2009 går reguleringsplanens bestemmelser foran kommunale normer ved eventuell motstrid.
+
+• Kommunale og private veier, adkomster og veibelysning skal stedstilpasses og planlegges og etableres i henhold til kommunens vei- og gatenorm.
+
+• Infrastruktur for renovasjon skal stedstilpasses og planlegges og etableres i henhold til kommunens renovasjonstekniske norm.
+
+• Infrastruktur for vann og avløp skal planlegges og etableres i henhold til Kommunens VA-norm.
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen skal sikre at teknisk infrastruktur planlegges og gjennomføres i tråd med dagens krav til sikkerhet, funksjonalitet og gjennomførbarhet. Mange eldre reguleringsplaner mangler tilstrekkelig detaljering av tekniske løsninger, eller bygger på standarder som ikke samsvarer med dagens krav til blant annet trafikksikkerhet, renovasjon, fremkommelighet, overvannshåndtering og VA-anlegg.
+
+Særlig gjelder dette eldre veier og adkomster som ikke er opparbeidet i tråd med reguleringsplanens forutsetninger, eller som har utilstrekkelig bredde og standard sett opp mot dagens behov. Bestemmelsen skal bidra til at tiltak innenfor eldre planområder opparbeides med en forsvarlig teknisk standard.
+
+For reguleringsplaner vedtatt før 01.07.2009 skal derfor kommunens tekniske normer legges til grunn ved planlegging og gjennomføring av tiltak. For reguleringsplaner vedtatt fra og med 01.07.2009 går reguleringsplanens bestemmelser foran kommunale normer ved eventuell motstrid.
+
+Krav om stedstilpasning skal samtidig sikre at tekniske løsninger tilpasses områdets karakter, terreng og eksisterende bebyggelsesstruktur, og at løsningene er samfunnsøkonomisk forsvarlige.
+      `,
+    },
+  ],
+} as const;
