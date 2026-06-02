910462b50d92a32854178a5e3f5ed9c00a1d0fd8
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:07:16 +0200

    Update kapittel11.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel11.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel11.ts
@@ -1,3 +1,72 @@
 export const kapittel11 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "11.1",
+      tittel: "Grad av utnyttelse",
+      bestemmelse: `For frittliggende småhusbebyggelse innenfor bestemmelsesområde GB skal bebygd areal (BYA) ikke overstige 25 % av tomtens nettoareal.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et likt grunnlag for lik utnyttelsesgrad i områder med frittliggende småhusbebyggelse innenfor GB.`,
+    },
+    {
+      nummer: "11.2",
+      tittel: "Høyde",
+      bestemmelse: `For boliger gjelder følgende høyder:
+
+Gesimshøyde skal ikke overstige 7,0 meter, og mønehøyde skal ikke overstige 9,0 meter, målt ut fra ferdig planert terrengs gjennomsnittsnivå rundt bygningen. Gesimshøyden skal likevel ikke overstige 8,0 meter fra laveste punkt på ferdig planert terreng.
+
+For garasjer og lignende tiltak gjelder følgende høyder:
+
+Gesimshøyde skal ikke overstige 3,0 meter og mønehøyde ikke overstige 5,0 meter. Høydene måles fra ferdig planert terrengs gjennomsnittsnivå rundt bygningen. Gesimshøyden skal likevel ikke overstige 4,0 meter fra laveste punkt på ferdig planert terreng.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et grunnlag for lik maksimal høyde i områder med frittliggende småhusbebyggelse innenfor GB.. Det vesentlige hensynet bak regelen er å sikre at området karakter videreføres, og særlig at hensynet til eksisterende kvaliteter som lys, gode uteoppholdsarealer, forholdet til eksisterende omgivelser mm. ivaretas i byggesaker. Bestemmelsen gir føringer på bruken av handlingsrommet i pbl. §29-1 og 29-2, som sammen med Arkitektur Asker forventes brukt aktivt i byggesaker.`,
+    },
+    {
+      nummer: "11.3",
+      tittel: "Boenheter",
+      bestemmelse: `På hver eiendom skal det ikke etableres mer enn ett av følgende alternativer:
+
+a.
+Én frittliggende enebolig med integrert sekundær boenhet.
+
+b.
+Én frittliggende enebolig med ett frittliggende mikrohus.
+
+c.
+Én tomannsbolig.
+
+Mikrohus skal være i én etasje, ha et bruksareal (BRA) på maksimalt 30 m2 og en mønehøyde på maksimalt 4,5 meter, jf. Sak10 §3-1 bokstav e.
+
+Ny sekundær boenhet skal være på maks 60m2 BRA, og integrert i eneboligen og underordnet hovedbruksenheten. Eksisterende godkjente sekundære boenheter over 60 m² skal ikke utvides. Sekundær boenhet kan likevel etableres i frittliggende bygg der dette følger av bestemmelse 8.2 om kulturminner.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et grunnlag for et likt maksimalt antall boenheter i områder med frittliggende småhusbebyggelse innenfor GB. Det vesentlige hensynet bak regelen er å sikre at området karakter videreføres, og at området ikke fortettes på en måte som endrer områdets karakter.
+
+Dette gjelder kun nye sekundære boenheter.`,
+    },
+    {
+      nummer: "11.4",
+      tittel: "Fradeling av boligtomt",
+      bestemmelse: `Ved fradeling av ny boligtomt skal minimum netto areal være:
+
+a.
+700m2 for enebolig, herunder enebolig med sekundær boenhet.
+
+b.
+1200m2 for tomannsbolig`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å stille et overordnet krav til tomtestørrelse ved etablering av nye boenheter i områder med frittliggende småhusbebyggelse innenfor GB. Det vesentlige hensynet bak regelen er å sikre at det ikke etableres nye boenheter på for små tomter. I enkeltsaker skal det også legges stor vekt på tomtens utforming når det skal tas stilling hvordan oppdeling/opprettelse av ny eiendom vil påvirke områdes karakter, herunder ved bruk av pbl. § 26-1 og § 29-2.
+
+Ved fradeling av eneboligtomt må tomten være egnet for alminnelig boligbebyggelse og ha en hensiktsmessig arrondering. Tomtestørrelsen må stå i forhold til tomtens beliggenhet, form og beskaffenhet, vernehensyn. Ny tomt tilpasses områdets typiske tomte- og bebyggelsesstruktur. Tomten bør ivareta krav til egnet uteoppholdsareal, atkomstforhold og parkering/garasjering. Tomten bør ikke være brattere enn et gjennomsnittlig stigningsforhold på 1:3. Terreng- og landskapstilpasning vektlegges, og kommunen kan med bakgrunn i dette kreve større tomter. Fradeling av eneboligtomt etter disse retningslinjene forutsettes normalt behandlet som mindre reguleringsendring.`,
+    },
+    {
+      nummer: "11.5",
+      tittel: "Terrengendringer utenfor byggverkets fotavtrykk",
+      bestemmelse: `Bygg skal plasseres i forhold til terreng og skal skje innenfor følgende rammer:
+
+a.
+For tomter med terrengfall mindre enn 1:3 skal høyde på støttemurer, fyllinger og/eller skjæringer ikke overskride 1 meter. Minste avstand mellom støttemurer skal være 1,5 meter i terrengets fallretning
+
+b.
+For tomter med terrengfall mer enn 1:3 skal høyde på støttemurer, fyllinger og/eller skjæringer ikke overskride 1,5 meter. Minste avstand mellom støttemurer skal være 2 meter i terrengets fallretning.
+
+c.
+Høyder måles fra eksisterende terreng. Avstand mellom støttemurer måles fra bunnen av øvre støttemur til toppen av nedre støttemur.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å sikre at det ikke gjøres for store inngrep i terrenget på enkeltomter, med hensyn til støttemurer, oppfylling, utfylling o.l. I enkeltsaker skal det også legges stor grad på opparbeidelsens virkninger for områdets karakter og de nærmeste omgivelsene.`,
+    },
+  ],
 } as const;
