6c1b9de130b5bd032f23bf2cfcfaaefda539d91c
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:16:48 +0200

    Update kapittel18.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel18.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel18.ts
@@ -1,3 +1,46 @@
+```ts id="q9m4xt"
 export const kapittel18 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "18.1",
+      tittel: "Småbåthavn",
+      bestemmelse: `Småbåthavners land- og bryggeanlegg skal være åpne for allmennhetens ferdsel til fots. Etablering av småbåthavn tillates kun der dette følger av reguleringsplan vedtatt etter 01.01.2010.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre nødvendige småbåthavner. Ved regulering av småbåthavner bør sikres gjesteplasser (minimum 10 % i større havner), og tilrettelegges for båtslipp der det er mulig, med allmenn tilgjengelighet. Landarealer og brygger skal være åpne for ferdsel, og det skal etableres løsninger for å redusere forurensning. Naturmangfold og kulturminner skal ivaretas, og brygger skal plasseres og utformes med hensyn til landskap, mudringsbehov og visuell eksponering.
+
+I detaljregulering skal det redegjøres for behov for båtplasser, mulighet for sanering av enkeltbrygger, økt tilgjengelighet til strandsonen, samt tilkobling til gang- og kollektivnett. Det skal også dokumenteres løsninger for teknisk infrastruktur, renovasjon, avløp og ladepunkter.
+
+For større tettstedsnære havner skal det i tillegg vurderes sambruk med sjøidretter, etablering av felles møteplasser, og hvordan parkering, trafikk, støy, vinteropplag og forholdet til naboer og allmenn ferdsel håndteres.`,
+    },
+    {
+      nummer: "18.2",
+      tittel: "Farled (hoved- og biled)",
+      bestemmelse: `I farled er det ikke tillatt med tiltak eller inngrep som vanskeliggjør, eller kommer i konflikt med, bruken som farled.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "18.3",
+      tittel: "Friluftsområde i sjø",
+      bestemmelse: `Innenfor området avsatt til friluftsområde i sjø skal det kun etableres tiltak som tilrettelegger for allmenhetens bruk av området.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "18.4",
+      tittel: "Akvakulturområde",
+      bestemmelse: `I områder avsatt til akvakulturformål skal det kun etableres tareanlegg med formål å forbedre miljøtilstanden i Oslofjorden. (beskrivelse, ulykker, hensyn til sjøliv)`,
+      spesialmerknad: `Hensikten med bestemmelsen er å bidra til positivt miljøtiltak for Oslofjorden gjennom etablering av tareanlegg.
+
+hydrologi/kapasitet ivaretas,
+
+erosjon/sediment kontrolleres,
+
+naturmangfold vurderes.`,
+    },
+    {
+      nummer: "18.5",
+      tittel: "Drikkevann",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+  ],
 } as const;
+```
