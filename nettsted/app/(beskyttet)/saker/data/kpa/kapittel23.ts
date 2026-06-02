581f31951f398f46c4cf987e1e0a161edbc03649
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:42:39 +0200

    Update kapittel23.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel23.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel23.ts
@@ -1,3 +1,48 @@
 export const kapittel23 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "23",
+      tittel: "Båndleggingssoner",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "23.1",
+      tittel: "Båndlagt areal etter lov om kulturminner, H740",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "23.1.1",
+      tittel: "Området er båndlagt i påvente av en avklaring om fredning etter Lov om kulturminner",
+      bestemmelse: `Området skal behandles som fredet inntil det foreligger endelig fredningsvedtak. Alle tiltak innenfor området skal godkjennes av Viken fylkeskommune.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "23.1.2",
+      tittel: "Automatisk fredet kirke og -gravplass etter kulturminneloven",
+      bestemmelse: `Det er ikke tillatt å iverksette tiltak som kan virke inn på den automatisk fredet kirke og gravplass med mindre det foreligger dispensasjon fra kulturminneloven. Innenfor middelaldergravplass er gravlegging kun tillatt i graver som har vært i kontinuerlig bruk etter 1945. Graver som ikke har vært i bruk etter 1945 skal ikke benyttes til gravlegging. Alle tiltak innenfor kirkestedet skal vurderes av kulturmiljøforvaltningen opp mot bestemmelsene i kulturminneloven. Ved all planlegging av tiltak`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "23.2",
+      tittel: "Båndlagt etter naturvernlovgivningen (naturmangfoldloven/naturvernloven), Hxx",
+      bestemmelse: `Innenfor Hxx, gjelder forskrifter vedtatt i medhold av naturmangfoldloven eller naturvernloven, som angitt i vedlegg X`,
+      spesialmerknad: `Hensikten med hensynssonen er å vise områder som er vernet etter naturvernlovgivningen og hvor den enkelte verneforskrift fastsetter selvstendige restriksjoner for tiltak, aktivitet og bruk. Vedlegg X til bestemmelsene gir en oversikt over verneforskrifters som gjelder i Asker kommune. For tiltak som er i strid med verneforskrifters bestemmelser må tiltakshaver eventuelt søke om dispensasjon fra verneforskriften. Dette gjelder uavhengig av kommunens behandling av byggesøknader etter plan- og bygningsloven.
+
+Når kommune mottar søknad om tiltak innenfor et område som er vernet ved forskrift etter naturvernlovgivningen, skal saken forelegges Statsforvalteren i Oslo og Viken for uttalelse.`,
+    },
+    {
+      nummer: "23.3",
+      tittel: "Båndlagt areal etter markaloven, H735",
+      bestemmelse: `Innenfor markagrensen vist på kommuneplanens arealplankart gjelder «Lov om naturområder i Oslo og nærliggende kommuner»`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "23.4",
+      tittel: "Båndlagt areal etter Energiloven, H740",
+      bestemmelse: `Det er ikke tillatt med ny bebyggelse innenfor hensynssonen.`,
+      spesialmerknad: ``,
+    },
+  ],
 } as const;
