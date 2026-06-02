b2fab8d788f3f4a525ec229d848af62db9864fb8
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:08:04 +0200

    Update kapittel12.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel12.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel12.ts
@@ -1,3 +1,87 @@
 export const kapittel12 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "12.1",
+      tittel: "Boligområder med spesielle kvaliteter (bestemmelsesområde # SB)",
+      bestemmelse: ``,
+      spesialmerknad: `Hensikten med spesielle boligområder er å sikre at områdenes helhetlige karakter og særpreg videreføres ved tiltak og fortetting. Bestemmelsene overstyrer reguleringsplaner som ikke videreføres som uendret og skaper motstrid til eldre planer.
+
+Bestemmelsene skal bidra til at nye tiltak tilpasses eksisterende bebyggelsesstruktur, herunder gjennom ensartede og dominerende takformer, tidstypisk utforming, detaljer og helhetlig arkitektonisk uttrykk, gjennomgående etasje- og høyderelasjoner, samt lav utnyttelsesgrad med tydelig grønn karakter. Samlet skal dette ivareta sammenhengen mellom bebyggelse, grønnstruktur og landskap, og sikre at områdenes arkitektoniske og landskapsmessige kvaliteter opprettholdes over tid.
+
+SB-områdene omfatter eksisterende småhusområder med tydelig og gjennomgående karakter.`,
+    },
+    {
+      nummer: "12.1.1",
+      tittel: "Grad av utnyttelse",
+      bestemmelse: `I områder avsatt til frittliggende småhusbebyggelse med spesielle kvaliteter (SB) skal bebygd areal (BYA) ikke overstige 20% av tomtens nettoareal.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et likt grunnlag for lik utnyttelsesgrad i boligområder med spesielle kvaliteter (SB). Bestemmelsen gir føringer på bruken av handlingsrommet i pbl. §29-1 og 29-2, som sammen med Arkitektur Asker forventes brukt aktivt i byggesaker.`,
+    },
+    {
+      nummer: "12.1.2",
+      tittel: "Høyder",
+      bestemmelse: `For boliger gjelder følgende høyder:
+
+Gesimshøyde skal ikke overstige 6,0 meter, og mønehøyde skal ikke overstige 9,0 meter, målt fra ferdig planert terrengs gjennomsnittsnivå rundt bygningen. Gesimshøyden skal likevel ikke overstige 7,0 meter fra laveste punkt på ferdig planert terreng.
+
+For garasjer og lignende tiltak gjelder følgende høyder:
+
+Gesimshøyde skal ikke overstige 3,0 meter, og mønehøyde skal ikke overstige 4,0 meter. Høydene måles fra ferdig planert terrengs gjennomsnittsnivå rundt bygningen. Gesimshøyden skal likevel ikke overstige 4,0 meter fra laveste punkt på ferdig planert terreng.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et grunnlag for lik maksimal høyde i SB-områder. Det vesentlige hensynet bak regelen er å sikre at området karakter videreføres, og særlig at hensynet til eksisterende kvaliteter som lys, gode uteoppholdsarealer, forholdet til eksisterende omgivelser mm. ivaretas i byggesaker. Bestemmelsen gir føringer på bruken av handlingsrommet i pbl. §29-1 og 29-2, som sammen med Arkitektur Asker forventes brukt aktivt i byggesaker.`,
+    },
+    {
+      nummer: "12.1.3",
+      tittel: "Boenheter",
+      bestemmelse: `I boligområde med spesielle kvaliteter skal det maksimalt etableres én frittliggende enebolig med én integrert sekundær boenhet per eiendom.
+
+Sekundær boenhet skal maks være 60m2 BRA i størrelse, og være integrert i eneboligen og underordnet hovedbruksenheten. Sekundær boenhet kan likevel etableres som frittliggende bygg der dette følger av bestemmelse 8.2 om kulturminner.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et grunnlag for et likt maksimalt antall boenheter i områder med frittliggende småhusbebyggelse. Det vesentlige hensynet bak regelen er å sikre at området karakter videreføres, og at området ikke fortettes på en måte som endrer områdets karakter. Hver tomt skal kun bebygges med 1 boenhet.
+
+Tidligere godkjente, eksisterende sekundære boenheter påvirkes ikke.`,
+    },
+    {
+      nummer: "12.2",
+      tittel: "Boligområder innenfor 100-metersbeltet langs sjø (bestemmelseområde B100)",
+      bestemmelse: ``,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å følge opp statlige planretningslinjer for differensiert forvaltning av strandsonen langs sjøen, hensynta natur- og kulturmiljø, friluftsliv, landskap/fjernvirkning og allmenne interesser. Bestemmelsen overstyrer reguleringsplaner som ikke videreføres som uendret.`,
+    },
+    {
+      nummer: "12.2.1",
+      tittel: "Grad av utnyttelse",
+      bestemmelse: `I områder avsatt til bolig innenfor 100-metersbelet langs sjø (B100) skal bebygd areal (BYA) ikke overstige 15% av tomtens nettoareal.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et likt grunnlag for lik utnyttelsesgrad i boligområder innenfor 100-metersbeltet langs sjø. Det vesentlige hensynet bak regelen er å sikre at områdenes karakter videreføres, og særlig at hensynet til eksisterende kvaliteter som natur, kulturmiljø og allmennhetens tilgang sikres og ivaretas i byggesaker. Bestemmelsen gir føringer på bruken av handlingsrommet i pbl. §29-1 og 29-2, som sammen med Arkitektur Asker forventes brukt aktivt i byggesaker.`,
+    },
+    {
+      nummer: "12.2.2",
+      tittel: "Høyder",
+      bestemmelse: `Gesimshøyde skal ikke overstige 4,5 meter, og mønehøyde skal ikke overstige 7.0 meter, målt fra gjennomsnittlig planert terreng rundt bygningen.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et grunnlag for lik maksimal høyde i boligområder i 100-metersbeltet langs sjø. Det vesentlige hensynet bak regelen er å sikre at området karakter videreføres, og særlig at hensynet til eksisterende kvaliteter som lys, gode uteoppholdsarealer, forholdet til eksisterende omgivelser mm. ivaretas i byggesaker.
+
+Bestemmelsen gir føringer på bruken av handlingsrommet i pbl. §29-1 og 29-2, som sammen med Arkitektur Asker forventes brukt aktivt i byggesaker.`,
+    },
+    {
+      nummer: "12.2.3",
+      tittel: "Boenheter",
+      bestemmelse: `På hver eiendom tillates kun én frittliggende enebolig.
+
+Sekundær boenhet kan etableres dersom følgende vilkår er oppfylt:
+
+a)
+Eiendommen har et netto tomteareal på minimum 1200 m2.
+
+b)
+Sekundær boenhet skal være integrert i hovedbygningen og kan ikke etableres som selvstendig frittliggende bygning.
+
+c)
+Unntak fra bokstav b gjelder der etablering i frittliggende bygning uttrykkelig tillates etter bestemmelse 7.xx om kulturminner.`,
+      spesialmerknad: `Hensikten med denne bestemmelsen er å gi et grunnlag for et likt maksimalt antall boenheter i områder med frittliggende småhusbebyggelse. Det vesentlige hensynet bak regelen er å sikre at området karakter videreføres, og at området ikke fortettes på en måte som endrer områdets karakter. Bestemmelsen skal sikre at området forblir et område med frittliggende eneboliger.`,
+    },
+    {
+      nummer: "12.2.4",
+      tittel: "Innenfor bestemmelsesområder xx for Nesøya",
+      bestemmelse: `Skal bebygd areal (BYA) ikke overstige 20% av tomtens nettoareal.
+
+Bebyggelse skal tilpasses landskapets karakter, eksisterende vegetasjon og sikre siktlinjer til åsene.`,
+      spesialmerknad: `Hensikten med bestemmelsesområdet er å sikre stedskvaliteter.`,
+    },
+  ],
 } as const;
