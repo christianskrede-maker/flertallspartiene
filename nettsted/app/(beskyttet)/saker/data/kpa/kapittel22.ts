66591e16e6215f596d148943ee5d3e8bab5a297e
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:20:38 +0200

    Update kapittel22.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel22.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel22.ts
@@ -1,3 +1,89 @@
 export const kapittel22 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "22",
+      tittel: "Hensynssone felles planlegging",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "22.1",
+      tittel: "Krav om samlet plan for Dikemarkområdet",
+      bestemmelse: `For Dikemark sentrumsområde, vist med feltbetegnelsen DI2 på kommuneplanens arealplankart, kan tiltak som nevnt i pbl. § 20-1, herunder opprettelse av ny grunneiendom, ikke finne sted før det er utarbeidet en samlet reguleringsplan for området.
+
+Område Lomannstangen, vist med feltbetegnelsen DI5 på kommuneplanens arealplankart, kan sammenføyes med sentrumsområde felt DI2 til en felles eiendom.
+
+For det sentrale sykehusområdet på Dikemark, vist med bestemmelsesområde #2 på kommuneplanens arealplankart, kan tiltak som nevnt i pbl. § 20-1, herunder opprettelse av ny grunneiendom, ikke finne sted før det er utarbeidet ny plan, enten som områderegulering eller detaljreguleringsplan.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "22.1.1",
+      tittel: "Krav om samlet plan for Holmen",
+      bestemmelse: `For området Holmen sentrum, vist med bestemmelsesområde # .. på kommuneplanens arealplankart, kan tiltak som nevnt i pbl. § 20-1, herunder opprettelse av nye grunneiendommer, ikke finne sted før det er utarbeidet ny plan, enten som områderegulering eller detaljreguleringsplan. I forkant av planen skal stedsanalyse og mulighetsstudie foreligge, som belyser helhetsgrep, avveier ulike interesser, definerer nødvendige rekkefølgekrav og gjennomføring av disse (herunder kostnader). Mulighetsstudien og påfølgende reguleringsplan skal definere en samlet arkitektonisk struktur for Holmen tettsted (basert på KDP Holmen-Slependen og Arkitektur Asker), og særlig avklare og fastsette løsninger for:
+
+•
+sentrale møteplasser, torg, badestrand,
+
+•
+parker, friluftsområder, kyststi, idrettsanlegg, leke- og aktivitetsarenaer,
+
+•
+samferdselsanlegg, veisystem (basert på KDP for ny E18), anlegg for kollektivtransport, hurtigbåtterminal, pendlerparkering, gang-/sykkelveier,
+
+•
+småbåthavn og småbåtanlegg,
+
+•
+teknisk infrastruktur (vann/avløp, overvann/avrenning til sjø, renovasjon).
+
+Hensynssone grunneierfinansiering kan vurderes som egnet virkemiddel for gjennomføring av infrastruktur innenfor bestemmelsesområdet.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "22.2",
+      tittel: "Krav om felles planlegging for Verket",
+      bestemmelse: `For de deler av Verket-halvøya som er vist med hensynssone H810_x, kreves samlet reguleringsplan før det kan gis tillatelse til etablering eller vesentlig endring av næringsvirksomhet, kaianlegg, større veianlegg eller tilhørende teknisk infrastruktur.
+
+Reguleringsplanen skal avklare samlet arealbruk, og ivareta det gjensidige hensyn mellom de ulike arealformål og interesser. Planen omfatter gjenbruk av dagens sandtaksområde, inkl. nytt anlegg for masseforvaltning, nye næringsvirksomheter og annen arealbruk som ivaretar hensynet til framtidig uttak av de stedlige mineralressurser. Planen skal tilrettelegge for ny kai for masse-/tungtransport og ny fergekai i Dramstadbukta, med tilhørende kulvert gjennom Ryggen og fylkesveiforbindelse. Følgelig skal dagens fylkesveitrasé forbi Verket dereguleres, og det skal sikres avvikling av dagens kaianlegg i Vollebukta og Svelviksundet. Planen skal vektlegge friluftsinteresser og naturinteresser, utvide friluftsområdet øst for Orografen på Ryggen og sikre at landskapet over kulvert reetableres.
+
+Reguleringsplanen skal særlig avklare og fastsette løsninger for:
+
+•
+miljøvirkninger, herunder støy, støv, forurensning, overvann/utslipp til sjø og virkninger for omkringliggende boliger/fritidsbolig og friluftsområder,
+
+•
+trafikk på sjø og vei, herunder transportomfang, trafikksikkerhet, kapasitet, konfliktpunkter og behov for samordning mellom sjøtransport og veitransport,
+
+•
+nytt kaianleggs plassering, dimensjonering, utforming, sikkerhet, sjøverts atkomst og samspill med øvrig arealbruk i området, samt avvikling av dagens kaianlegg i Vollebukta og Svelviksundet,
+
+•
+kulvert gjennom LNF-område for atkomst til kaianlegg for både massetransport, almen biltrafikk, syklister og fotgjengere, herunder plassering, dimensjonering, utforming, sikkerhet, teknisk gjennomførbarhet, reetablering av landskap/ landskapstilpasning,
+
+•
+friluftsliv og allmennhetens tilgang til sjøen, herunder sammenheng, plassering og kvalitet for kyststien,
+
+•
+istandsetting og restaurering av berørte LNF-arealer, herunder landskap, terrengformer og vegetasjonsbilde, tiltak for å gjenopprette naturverdier, både til lands og i fjorden,
+
+•
+intern logistikk, massestrømmer, arealbehov for mellomlagring, drift og skjerming, og mulige synergier mellom masseforvaltning, næringsvirksomhet og sjøtransport, herunder løsninger som kan redusere samlet miljø- og transportbelastning,
+
+•
+rekkefølge for gjennomføring av tiltak, definere nødvendige avbøtende tiltak og rekkefølgekrav og gjennomføring av disse (herunder kostnader).`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "22.3",
+      tittel: "Krav om samlet plan for Øvre Hallenskog",
+      bestemmelse: `For området på Øvre Hallenskog, vist med hensynssone # .., skal tiltak som nevnt i pbl. § 20-1, herunder opprettelse av nye grunneiendommer, ikke finne sted før det er utarbeidet ny samlet plan, enten som områderegulering eller detaljreguleringsplan, som ivaretar rekkefølgebestemmelser knyttet til etablering av ny hovedatkomstvei, jf. reguleringsplan for Øvre Hallenskog – atkomstvei, vedtatt 06.05.2020, samt en helhetlig løsning for grønnstruktur og teknisk infrastruktur.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "22.4",
+      tittel: "Pågående planprosess for event. sprengstoffanlegg i Hurummarka/Oredalen",
+      bestemmelse: `I området Hurummarka/Oredalen, vist med bestemmelsesområde # .., pågår i regi av Staten et reguleringsarbeid for tilrettelegging av et event. sprengstoffanlegg. Tiltak som nevnt i pbl. § 20-1, herunder opprettelse av nye grunneiendommer, kan ikke finne sted i området uten at det foreligger tillatelse fra planmyndigheten (Kommunal- og distriktsdepartementet).`,
+      spesialmerknad: ``,
+    },
+  ],
 } as const;
