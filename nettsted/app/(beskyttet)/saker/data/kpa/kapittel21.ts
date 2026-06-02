ea2d29ba0be3358af8f0d9454c0222bbc4972ad2
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:19:27 +0200

    Update kapittel21.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel21.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel21.ts
@@ -1,3 +1,16 @@
+```ts id="j5r8nm"
 export const kapittel21 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "21",
+      tittel: "Hensynssone infrastruktur – flomveier",
+      bestemmelse: `Naturlige flomveier skal ivaretas innenfor hensynssonen. Flomveier skal utformes slik at vannet transporteres bort uten å gjøre skade nedstrøms. Det skal sikres trygg avrenning fra byggeområder til naturlige flomveier. Tørrbekker skal holdes åpne og fungere som flomveier. Bygninger og anlegg ved flomveier skal utformes slik at naturlige flomveier ivaretas.
+
+Det skal avsettes areal for nye flomveier ved planlegging og søknad om tiltak som berører eksisterende flomveier. Ved etablering av flomveier skal omkringliggende arealer, som bygninger og annen infrastruktur, sikres mot flomskader. Flomveier, drenslinjer, flomsoner, erosjon og stormflo fremgår av aktsomhetskart i kommunes kartløsning og skal legges til grunn i plan- og byggesaker.`,
+      spesialmerknad: `Hensikten med hensynssonen er å sikre naturlige flomveier for å redusere risikoen for uønskede hendelser knyttet til flom.
+
+Det må redegjøres for flomveier i reguleringsplanen og sikre avbøtende tiltak i planen. Flomveier og drenslinjer vurderes spesielt ved bratt terreng.`,
+    },
+  ],
 } as const;
+```
