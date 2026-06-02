f4249ff1a17fa2764905b93f3f6358b21de82d46
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:10:27 +0200

    Update kapittel14.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel14.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel14.ts
@@ -1,3 +1,62 @@
 export const kapittel14 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "14.1",
+      tittel: "Næringsformål NA1",
+      bestemmelse: `Innenfor næringsbebyggelse NA1 skal arealformål begrenses til næring og detaljhandel.`,
+      spesialmerknad: `Hensikt med bestemmelsen er å etablere arbeidsintensive virksomheter med mange ansatte og besøkende, og tilrettelegge for blant annet detaljhandel og større kontorvirksomheter.
+
+Innenfor kategori A i abc-prinsippet.`,
+    },
+    {
+      nummer: "14.2",
+      tittel: "Næringsformål NA2",
+      bestemmelse: `Innenfor næringsbebyggelse NA2 skal arealformål begrenses til næring og offentlig eller privat tjenesteyting. Detaljhandel tillates ikke.`,
+      spesialmerknad: `Hensikt med bestemmelsen er å etablere arbeidsintensive virksomheter med mange ansatte og besøkende, blant annet større kontorvirksomheter, samt åpne opp for å etablere bebyggelse for offentlig eller privat tjenesteyting.
+
+Innenfor kategori A i abc-prinsippet.`,
+    },
+    {
+      nummer: "14.3",
+      tittel: "Næringsformål NB1",
+      bestemmelse: `Innenfor næringsbebyggelse NB1 skal arealformål begrenses til næring.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å etablere transportbedrifter, verksteder, lagerhaller, bygg- og anleggsvirksomheter, ikke-støyende industrivirksomheter samt plasskrevende varehandel.
+
+Innenfor kategori B i abc-prinsippet.`,
+    },
+    {
+      nummer: "14.4",
+      tittel: "Næringsformål NB2",
+      bestemmelse: `Innenfor næringsbebyggelse NB2 skal arealformål begrenses til næring.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å etablere verksteder, transportbedrifter, lagerhaller og samleterminal, bygg- og anleggsvirksomheter, samt ikke-støyende industrivirksomheter.
+
+Innenfor kategori B i abc-prinsippet.`,
+    },
+    {
+      nummer: "14.4.1",
+      tittel: "Kystkultursenter",
+      bestemmelse: `Innenfor område avsatt som NB2.1 på Nærsnes skal det etableres kystkultursenter.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "14.5",
+      tittel: "Næringsformål NC",
+      bestemmelse: `Innenfor næringsbebyggelse NC skal det kun etableres industrivirksomheter.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å etablere industrivirksomhet.
+
+Innenfor kategori C i abc-prinsippet.`,
+    },
+    {
+      nummer: "14.6",
+      tittel: "Næringsformål ND",
+      bestemmelse: `Innenfor næringsbebyggelse ND kan det etableres virksomheter som legger til rette for reiseliv, som hotell, servering og annen næringsvirksomhet.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å skape forutsigbarhet og stimulere til etablering av virksomheter som legger til rette for reiseliv, som hotell, servering og annen næringsvirksomhet.`,
+    },
+    {
+      nummer: "14.7",
+      tittel: "Næringsformål NE",
+      bestemmelse: `Innenfor næringsbebyggelse NE kan det etableres industrielle aktiviteter, aktiviteter for bevaring og formidling av områdets kulturelle og industrielle historie, kontorer og andre egnede næringsaktiviteter i sammenheng med dette.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å tilrettelegge for og stimulere til etablering av industrielle aktiviteter, aktiviteter for bevaring og formidling av områdets kulturelle og industrielle historie, kontorer og andre egnede næringsaktiviteter i sammenheng med dette.`,
+    },
+  ],
 } as const;
