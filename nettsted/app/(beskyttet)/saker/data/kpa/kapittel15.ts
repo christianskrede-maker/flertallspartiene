7562cc61bd8602a516e0abc3c2150f10a83a7bb3
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:12:59 +0200

    Update kapittel15.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel15.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel15.ts
@@ -1,3 +1,155 @@
 export const kapittel15 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "15.1",
+      tittel: "Bebyggelse og anlegg (BA)",
+      bestemmelse: `Områder for bebyggelse og anlegg åpner for kombinert nærings- og boligformål.`,
+      spesialmerknad: `Formålet bebyggelse og anlegg benyttes der det, utenfor senterstrukturen/sentrumsformål, åpnes for kombinasjon av nærings- og boligformål. Næringsformålene er inndelt i underformål, i den hensikt å sikre at ulike kategorier næring lokaliseres på hensiktsmessig sted, Jf. kommuneplanens samfunnsdel og prinsippet om «rett virksomhet på rett sted» (ABC-prinsippet).`,
+    },
+    {
+      nummer: "15.1.1",
+      tittel: "Byggeformål BA1",
+      bestemmelse: `Innenfor BA1 skal arealformål begrenses til boligbebyggelse i kombinasjon med ett eller flere av følgende formål; kontor, verksted og lager og bygg- og anleggsvirksomhet. Eksisterende detaljhandelsvirksomhet skal ikke utvides utover eksisterende omfang.
+
+Innenfor bestemmelsesområde xx (Føyka)
+
+Minimum 50% av Føyka-området (regulert delfelt I, II og IV) skal være fri for bebyggelse og tilrettelagt for rekreasjon og fysisk aktivitet.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.1.2",
+      tittel: "Byggeformål BA2",
+      bestemmelse: `Innenfor BA2 skal formål begrenses til boligbebyggelse i kombinasjon med kontor, verksted og lager, bygg og anleggsvirksomhet, samt arealer for plasskrevende varehandel.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.1.3",
+      tittel: "Byggeformål BA3",
+      bestemmelse: `Innenfor BA3 skal formål begrenses til boligbebyggelse i kombinasjon med kontor, verksted og lager, bygg og anleggsvirksomhet.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.1.4",
+      tittel: "Byggeformål BA4",
+      bestemmelse: `Innenfor BA4 skal formål begrenses til bebyggelse for offentlig eller privat tjenesteyting i kombinasjon med boligbebyggelse.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.1.4.1",
+      tittel: "Byggeformål BA4.1 Sosiale boformer (felt SL14, felt ÅN3)",
+      bestemmelse: `Innenfor BA4.1 skal det etableres offentlig eller privat tjenesteyting i kombinasjon med boliger knyttet til sosiale boformer for eldre, eller personer med nedsatt funksjonsevne.
+
+Øvre Hagen gård, felt ÅN3, tillates bygget om til boliger innenfor eksisterende bebyggelse. Gårdstunet skal opprettholdes og anlegget tilpasses kulturlandskapet. Eksisterende terreng og vegetasjon skal inngå som et aktivum i bofelleskapet. Nødvendige tiltak skal iverksettes slik at tilstøtende landbruksarealer skjermes og landbruksdrift kan pågå uforstyrret.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.1.4.2",
+      tittel: "Byggeformål BA4.3 (Tronstad)",
+      bestemmelse: `Innenfor BA4.3 skal det etableres boligbebyggelse i kombinasjon med bebyggelse for offentlig eller privat tjenesteyting, herunder museumsvirksomhet.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.2",
+      tittel: "Sentrumsformål",
+      bestemmelse: `I områder avsatt til sentrumsformål skal himlingshøyde i bebyggelsens første etasje langs strøksgater ha høyde minimum 4,5 meter, og arealbruken skal være utadvendt.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre gode og levende sentrumsområder. Gater (kommunal vei og fylkesvei) gjennom sentrumsformål skal primært utformes med gatepreg og inkludere gode løsninger for busstransport, samt separate anlegg for syklister og fotgjengere. Det skal legges særskilt vekt på trafikksikre løsninger for fotgjengere.
+
+Sentrumsområder ved kollektivknutepunkt skal utvikles med utgangspunkt i stedets særpreg og i dialog med lokalmiljøet. Fortetting gir ikke i seg selv føringer for volum og høyde, som avklares i reguleringsplan.
+
+Ved regulering skal sentrumsområder bidra til å bygge opp under inkluderende og levende lokalsamfunn, med vekt på sosial struktur, aldersvennlige løsninger og varierte møteplasser for ulike brukergrupper.
+
+Det skal legges til rette for robuste og fleksible bygg med lang levetid og mulighet for ombruk. Førsteetasjer skal utformes for utadrettede funksjoner og bidra til aktive byrom, samtidig som de sikrer fleksibel arealbruk og framtidsrettede tekniske løsninger.
+
+Asker kommunes arkitekturstrategi skal brukes for å sikre gode kvaliteter.`,
+    },
+    {
+      nummer: "15.3",
+      tittel: "Fritids- og turistformål (FC og FT)",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.3.1",
+      tittel: "1FC (Ramton, Solbergstøa, Knattvoldstranda og Rødtangen)",
+      bestemmelse: `Innenfor FC tillates campingplass med tilhørende service.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.3.2",
+      tittel: "FT (Verpen)",
+      bestemmelse: `Innenfor FT tillates utleiehytter og annen turistnæring`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.4",
+      tittel: "Idrettsformål",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.4.1",
+      tittel: "Avgrunnsdalen",
+      bestemmelse: `Området kan tillates benyttet til skyteanlegg med tilknyttede aktiviteter. Området skal være åpent for allmenheten når anlegget ikke er i aktiv bruk.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.4.2",
+      tittel: "Vardåsen",
+      bestemmelse: `Innenfor området kan det etableres idrettsanlegg under forutsetning av at:
+
+a.
+Eksisterende stier ivaretas i reguleringsplan
+
+b.
+Det ikke foretas inngrep i Barlindbekken
+
+c.
+Kartlagte naturtyper i området sikres mot inngrep gjennom reguleringsplan`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.5",
+      tittel: "Råstoffutvinning",
+      bestemmelse: `Områder som er avsatt tilråstoffutvinning tillates ikke bebygget eller benyttet på en måte som vanskeliggjør framtidigutvinning av mineralske ressurser.`,
+      spesialmerknad: `Hensikten er å sikre mulig fremtidig uttak av mineralske ressurser.`,
+    },
+    {
+      nummer: "15.6",
+      tittel: "Andre typer anlegg (ATA)",
+      bestemmelse: ``,
+      spesialmerknad: `Hensikten med bestemmelsene er å sikre arealer for masseforvaltning.
+
+Ved regulering av områder for masseforvaltning bør det vurderes å avsette arealer til snødeponi. Dette kan særlig være aktuelt innenfor områder avsatt til massehåndtering (f.eks. ATA1, ATA1.1, ATA2 og NB2), da disse ofte har avklarte forhold knyttet til avrenning, støy og trafikk.
+
+Etablering av snødeponi forutsetter at reguleringsplan åpner for slik bruk, samt tillatelse fra forurensningsmyndighet. Ved regulering skal det vurderes avbøtende tiltak knyttet til støy, forurensning, håndtering av smeltevann og trafikk. Det skal også vurderes om tiltaket medfører økt støy eller trafikk som utløser behov for ny regulering.`,
+    },
+    {
+      nummer: "15.6.1",
+      tittel: "Byggeformål ATA.1",
+      bestemmelse: `Innenfor ATA1 tillates uttak og mottak av masser, deponi, samt arealer for mellomlagring og gjenbruk av masser. Mottak og deponi av masser, og eventuelle bygningsmessige tiltak, skal ikke vanskeliggjøre framtidig utvinning av mineralske ressurser innenfor området.
+
+• Byggeformål ATA1.2 Follestad: Området kan kombineres med idrettsformål.
+
+• Byggeformål ATA1.3 Stokkeråsen: Innenfor ATA1.3 kan det deponeres snø, under forutsetning av at området reguleres.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.6.2",
+      tittel: "Byggeformål ATA2",
+      bestemmelse: `Innenfor ATA2 kan arealbruk begrenses til mottak av masser, deponi, samt arealer for mellomlagring og gjenbruk av masser.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.6.3",
+      tittel: "Byggeformål ATA3",
+      bestemmelse: `ATA3 er avsatt til areal for transformatorstasjoner.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "15.6.4",
+      tittel: "Byggeformål ATA4",
+      bestemmelse: `ATA4 er avsatt til areal for infrastrukturanlegg.`,
+      spesialmerknad: ``,
+    },
+  ],
 } as const;
