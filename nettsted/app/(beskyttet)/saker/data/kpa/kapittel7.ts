6ac62270c079ebf5dedc82471b8d05dd78efa269
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 19:50:13 +0200

    Update kapittel7.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel7.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel7.ts
@@ -1,3 +1,140 @@
 export const kapittel7 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "7.1",
+      tittel: "Lysforurensning",
+      bestemmelse: `Utendørs belysning skal begrenses til det som er nødvendig for sikkerhet og funksjon. Belysning skal utformes, avskjermes og driftes slik at blending, strølys og unødig lysforurensning unngås. Belysning skal som hovedregel være nedadrettet og behovsstyrt.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å redusere lysforurensning ved at utendørsbelysning er nedadrettet og avskjermet slik at direkte lys ikke rettes mot himmel, sjø, naboeiendom eller offentlig vei. Plasseringen og utformingen av lysmaster og annen utendørs belysning bør være slik at blending og strølys mot omkringliggende bebyggelse og naturområder begrenses.
+
+Videre er hensikten at belysning av fasader, terreng, vegetasjon og uteoppholdsarealer skal være behovsstyrt, for eksempel med tidsur, sensor eller tilsvarende løsning som begrenser driftstid utenom faktisk bruk. Fastmontert utendørs belysning bør ha varm fargetemperatur på maksimalt 3000 kelvin.`,
+    },
+    {
+      nummer: "7.2",
+      tittel: "Støy- og luftforurensing",
+      bestemmelse: ``,
+      spesialmerknad: `Hensikten med bestemmelser er å sikre at nødvendige utredninger, avveiinger og avbøtende tiltak foretas og fastsettes gjennom støyfaglig utredning. Nødvendige avbøtende tiltak mot støy skal etableres dersom støyfaglig vurdering viser at grenseverdier eller krav til støyforhold i bestemmelsene ikke oppfylles. Tiltakene skal være gjennomført før det kan gis brukstillatelse eller ferdigattest.
+
+Støyfaglig utredning skal dokumentere at støymessige forhold er ivaretatt i henhold til retningslinje for behandling av støy i arealplanlegging T-1442, og at grenseverdier i T-1442, tabell 2 tilfredsstilles.
+
+Retningslinje T-1442 for støy i arealplanlegging gjelder også for bygge- og anleggsfasen for søknadspliktige tiltak jf. pbl. § 20-1. Der det kan forventes overskridelse av støygrensene ved bygge- og anleggsarbeid skal det utarbeides en plan for håndtering av støy i bygge- og anleggsfasen. Arbeid som overskrider støygrensene i tabell 4 i T-1442 skal ikke skje om natten.`,
+    },
+    {
+      nummer: "7.2.1",
+      tittel: "Grunnlag for regulering og søknad om tiltak",
+      bestemmelse: `Ved regulering og søknad om tiltak skal:
+
+Ved regulering og søknad om tiltak skal retningslinje T-1520 om luftkvalitet i arealplanlegging og retningslinje T-1442 om støy i arealplanlegging legges til grunn.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "7.2.2",
+      tittel: "Støysonekart",
+      bestemmelse: `Kommunens støysonekart for vei og jernbane er grunnlag for vurdering av støysone der det ikke foreligger egen støyfaglig utredning for området. Slik utredning må ikke være eldre enn 10 år.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "7.2.3",
+      tittel: "Gul støysone - vurderingssone",
+      bestemmelse: `Ny bebyggelse
+
+Nye bygninger til støyfølsomt bruksformål kan oppføres i områder med støynivå opptil Lden 65 dB fra vei og Lden 68 dB fra jernbane, jf. T-1442, dersom følgende vilkår er oppfylt:
+
+a.
+Minste uteoppholdsareal (MUA) for boliger, skoler og barnehager skal ha støynivå innenfor grenseverdiene i T-1442 tabell 2.2
+
+b.
+Alle boenheter skal ha minst én stille side med støynivå opptil Lden 55 dB utenfor fasade.
+
+c.
+Minst 50 % av rom til støyfølsomt bruk skal ha vindu mot stille side med støynivå opptil Lden 55 dB utenfor vindu.
+
+d.
+Minst 1 soverom i hver boenhet skal ha vindu mot stille side.
+
+Endringer av eksisterende bebyggelse
+
+Eksisterende bygninger med støyfølsomt bruksformål innenfor gul støysone kan utvides, påbygges og bruksendres dersom:
+
+a. Tiltaket ikke etablerer ny boenhet, med mindre kravene for ny bebyggelse er oppfylt. Sekundærboenhet skal oppfylle samme støykrav som hovedbruksenheten.
+
+b. Nye soverom skal ha vindu mot stille side med støynivå opptil Lden 55 dB utenfor vindu.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "7.2.4",
+      tittel: "Rød støysone – forbudssone",
+      bestemmelse: `Ny bebyggelse
+
+Ny bebyggelse med støyfølsomt bruksformål og opprettelse av ny grunneiendom til slikt formål er ikke tillatt i rød støysone.
+
+Endringer av eksisterende bebyggelse
+
+Eksisterende bebyggelse med støyfølsomt bruksformål i rød støysone kan ikke gjenoppbygges, utvides, påbygges eller bruksendres til økt støyfølsom bruk. Nye boenheter er ikke tillatt.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "7.2.5",
+      tittel: "Avviksområder for støy",
+      bestemmelse: `Innenfor avvikssone for støy, vist med bestemmelsesområde #1 på kommuneplanens arealplankart, kan ny bebyggelse med støyfølsomt bruksformål (med unntak av nye skoler og barnehager) oppføres og eksisterende bebyggelse med støyfølsomt bruksformål utvides og påbygges i områder med støynivå utenfor vindu i rom for varig opphold opp til Lden 70 dB fra vei og Lden 73 dB fra jernbane, jf. T-1442 på følgende vilkår:
+
+i.
+Minste uteoppholdsareal (MUA) for skoler, barnehager og boliger skal ikke ha støynivå over grenseverdier i T-1442, tabell 2.2
+
+ii.
+Alle boenheter (primære og sekundære) har en stille side (støynivå opp til Lden 55 dB utenforfasade)
+
+iii.
+Minimum 50 % av antall rom til støyfølsom bruk skal ha vindu mot stille side (støynivå opptil Lden 55 dB utenfor vindu)
+
+iv.
+d. Minimum 1 soverom (i hver boenhet) skal ha vindu mot stille side (støynivå opp til Lden 55 dB utenfor vindu`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "7.3",
+      tittel: "Vassdragsmiljø og flomveier",
+      bestemmelse: ``,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "7.3.1",
+      tittel: "Sikring av kantvegetasjon langs vassdrag",
+      bestemmelse: `Langs elver/bekker med årssikker vannføring, tjern, innsjøer og dammer skal naturlig kantvegetasjon bevares og opprettholdes i en sone på minst 6 meter.
+
+Avstanden måles horisontalt fra elve- eller bekkekant ved normal vannføring.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å ivareta naturlige vegetasjonsbelter langs vassdrag og vann av hensyn til kantvegetasjonens funksjon for biologisk mangfold. Naturlige vegetasjonsbelter langs vassdrag og vann bidrar også til å hindre avrenning og motvirke erosjon. Hvordan dette ivaretas detaljeres ved regulering.`,
+    },
+    {
+      nummer: "7.3.2",
+      tittel: "Tiltak som påvirker vassdragmiljø",
+      bestemmelse: `Lukking, utfylling, kanalisering, steinsetting eller andre tiltak som vesentlig endrer vassdragets naturlige løp, kantsoner eller økologiske funksjon, er ikke tillat.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å beskytte miljøtilstanden i vassdrag. Bestemmelsen skal sikre at åpne strekninger av elver, bekker, vann og dammer opprettholdes, og at tiltak ikke påvirker miljøtilstanden i vannforekomsten negativt.
+
+Ved en eventuell dispensasjonsvurdering etter pbl. § 19-2, skal det foretas en vurdering av om tiltaket kan føre til en forringelse av vannforekomsters økologiske tilstand/potensial eller at miljømålet fastsatt for vannforekomsten i gjeldende regionale vannforvaltningsplan ikke nås, og eventuelt vurdere om vilkårene i vannforskriften § 12 er oppfylt for fysiske endringer i vannforekomster.
+
+Fysiske tiltak i vassdrag krever særskilt tillatelse etter forskrift om fysiske tiltak i vassdrag. Fysiske tiltak som innebærer fjerning av kantvegetasjon kan være i strid med vannressursloven § 11, og dermed kreve disposisjon fra statsforvalteren.`,
+    },
+    {
+      nummer: "7.4",
+      tittel: "Tilpasning av bygg til terreng og landskap",
+      bestemmelse: `Nye bygg skal tilpasses eksisterende terreng, topografi og landskap. For å unngå større fyllinger og skjæringer ved plassering i terreng, skal høydeforskjellen på tomten tas opp i bebyggelsen.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre kommunen et grunnlag for å stille krav til tilpasning av bygg til terreng og landskap i reguleringssaker og i byggesaker. For reguleringssaker vil bestemmelsen gi et krav om at dette behandles og fremstilles når det fremmes planinitiativ, eller når planforslag legges ut på høring.
+
+I byggesaker gir bestemmelsen et grunnlag for bruk av handlingsrommet i pbl. § 29-1 og 29-2.
+
+Hensikten er at tiltak skal utformes med utgangspunkt i stedets landskap, topografi, eksisterende kvaliteter og særpreg. Terrenginngrep, masseuttak, skjæringer, fyllinger og støttemurer skal begrenses, og tiltak skal bidra til helhetlige og sammenhengende bygde omgivelser. Som hovedregel skal halvparten av tomtens eksisterende topografi og naturlig vegetasjon bevares, store avvik fra dette tillates ikke. Terrengendringer som fører til at tomten er ugjenkjennbar tillates ikke.
+
+Med kravet om at «høydeforskjellen skal tas opp i tomten» ligger det at det er bygget som skal tilpasse seg tomten, og ikke motsatt.`,
+    },
+    {
+      nummer: "7.5",
+      tittel: "Arkitektonisk kvalitet, stedlig tilpasning og utforming",
+      bestemmelse: `Ved regulering og behandling av byggetiltak skal Arkitektur Asker (dato) legges til grunn for vurdering av arkitektonisk kvalitet, stedstilpasning og utforming av bebyggelse, anlegg og utearealer.`,
+      spesialmerknad: `Hensikten med bestemmelsen er at bebyggelse, anlegg og utearealer skal utformes i tråd med prinsippene i kommunens arkitekturstrategi.
+
+Arkitektur Asker er kommunens arkitekturstrategi og faglige grunnlag for vurdering av arkitektonisk kvalitet, stedstilpasning og visuelle kvaliteter i planarbeid. Strategien skal brukes som dialog- og vurderingsverktøy ved utarbeidelse og behandling av reguleringsplaner og ved byggetiltak. Strategien angir hensyn som kommunen legger vekt på i planfaglige vurderinger. Bebyggelse, anlegg og utearealer skal utformes med arkitektonisk kvalitet og tilpasses stedets landskap, terreng, bebyggelsesstruktur, kulturmiljø og offentlige rom.
+
+Hensikten er videre at tiltak skal tilpasses stedets naturlige landskap og bygde omgivelser, terreng, bebyggelsesstruktur, kulturmiljø og offentlige rom, og bidra til helhetlige omgivelser med gode visuelle kvaliteter.`,
+    },
+  ],
 } as const;
