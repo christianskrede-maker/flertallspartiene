e8a132f0b324f2b6945f3c12a70c900de6b655e8
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:09:21 +0200

    Update kapittel13.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel13.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel13.ts
@@ -1,3 +1,40 @@
 export const kapittel13 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "13.1",
+      tittel: "Fritidsbebyggelse utenfor 100-metersbeltet langs sjø",
+      bestemmelse: `Det skal kun etableres én fritidsbolig på eksisterende ubebygget tomt og utvidelse av eksisterende fritidsbebyggelse i områder avsatt til fritidsbebyggelse eller kombinert bolig- og fritidsbebyggelse felt KBF, under følgende forutsetninger:
+
+a.
+Tilknytning til vann og avløp. Fritidsboligen skal tilknyttes godkjent vann- og avløpsanlegg.
+
+b.
+Utvidelse av eksisterende fritidsbolig kan ikke medføre opprettelse av ny boenhet på tomten. Grad av utnytting skal ikke overskride %-BYA = 15 % av tomtens nettoareal, og samlet BYA skal overskrider ikke 125 m².
+
+c.
+Høyder. Gesimshøyde tillates inntil 4,0 meter og mønehøyde tillates inntil 6,0 meter, målt fra ferdig planert terrengs gjennomsnittsnivå rundt bygningen. Gesimshøyden skal likevel ikke på noe punkt overstige 5 m fra laveste punkt på ferdig planert terreng.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å primært å etablere helhetlige rammer for individuell utbygging av fritidsbebyggelse.
+
+Bruksendring fra fritidsbolig til bolig er spesielt aktuelt i KBF områdene, fire konkrete områdene, Brønnøya, Storsand, Holmsbu og Torgrimsåsen hvor det allerede er boligbebyggelse. Der ny bruk vil være i strid med regulert arealformål, kreves ny reguleringsplan eller endring av gjeldende plan.`,
+    },
+    {
+      nummer: "13.2",
+      tittel: "Fritidsbebyggelse beliggende i 100-metersbeltet, utenfor byggegrense mot sjø",
+      bestemmelse: `Det tillates ett tilbygg på inntil 8 m² til eksisterende fritidsbebyggelse i områder avsatt til kombinert bolig- og fritidsbebyggelse (KBF) og fritidsbebyggelse beliggende i 100-metersbeltet, utenfor byggegrense mot sjø, når følgende vilkår er oppfylt:
+
+1.
+tilbygget er nødvendig for å sikre avkloakkering av eiendommen ved tilkobling til godkjent vann- og avløpsanlegg,
+
+2.
+det kan dokumenteres at sanitæranlegg ikke kan integreres i eksisterende bebyggelse,
+
+1.
+tilbygget plasseres i tilknytning til hovedbruksenheten, i retning bort fra sjø.`,
+      spesialmerknad: `Hensikten med bestemmelsen er ment som en snever unntaksbestemmelse for å kunne etablere nødvendige sanitæranlegg i eksisterende fritidsbebyggelse hvor dette ikke lar seg løse innenfor eksisterende bygningsmasse. Vurderingen av nødvendighet må foretas konkret i den enkelte sak.
+
+Det er søker/tiltakshaver som må fremme en begrunnet søknad og redegjøre for hvorfor det er nødvendig med den prosjekterte størrelsen på tilbygget. At bestemmelsen åpner for tilbygg på inntil 8 m² innebærer ikke at det automatisk anses nødvendig med et tilbygg av denne størrelsen. Tiltaket skal begrenses til det arealet som er nødvendig for å etablere sanitæranlegget på en funksjonell og forsvarlig måte.
+
+Tilbygget skal kun benyttes til sanitærfunksjoner knyttet til bad/wc. Det tillates ikke at arealet benyttes til gangareal, oppholdsrom, bod eller andre formål utover det som er nødvendig for sanitæranlegget.`,
+    },
+  ],
 } as const;
