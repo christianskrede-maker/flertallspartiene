fe1f9236c0a81b8cae276e78c4c8670ff18224d0
christianskrede-maker <christianskrede@gmail.com>
2026-06-02 20:15:18 +0200

    Update kapittel17.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel17.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel17.ts
@@ -1,3 +1,85 @@
 export const kapittel17 = {
-  deler: [],
+  deler: [
+    {
+      nummer: "17.1",
+      tittel: "Fellesbestemmelser LNFR- områder",
+      bestemmelse: `Tiltak i LNFR-områder skal ikke medføre nedbygging av dyrket eller dyrkbar mark, inngrep i verdifull skog eller redusere allmennhetens tilgang til utmark.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å sikre jordbruk og naturverdier i LNF-områder. Naturrestaurerende og naturforbedrende tiltak er i utgangpunktet positivt. Dette gjelder også masseoppfylling av rene naturlige masser som forbedrer dyrking, miljø og landskapsmessige kvaliteter.
+
+LNFR-områder skal i hovedsak benyttes til landbruk, natur- og friluftsformål og skal i utgangspunktet være ubebygd, med unntak for tiltak knyttet til stedbunden næring og allment friluftsliv.
+
+Kårbolig kan tillates når det er nødvendig for landbruksdriften, det kun er én eksisterende bolig på eiendommen, og boligen plasseres som del av gårdstunet. Fradeling tillates ikke.
+
+Gjenoppbygging av bolig kan vurderes dersom plassering, volum og høyder i hovedsak opprettholdes, med mulighet for justering dersom dette gir bedre landskaps- eller strandsonetilpasning.
+
+Jordlovens bestemmelser om vern av dyrka og dyrkbar mark gjelder fram til reguleringsplan er vedtatt, og delvis fram til byggetillatelse er gitt.
+
+Snødeponi kan vurderes regulert innenfor LNFR der dette ikke kommer i konflikt med landbruk, vassdrag, natur- eller kulturmiljøverdier.`,
+    },
+    {
+      nummer: "17.1",
+      tittel: "Fellesbestemmelser LNFR- områder",
+      bestemmelse: `Offentlig eller privat vann- og avløpsanlegg kan etableres for å forsyne eksisterende hovedbruksenhet. Anlegget skal ikke berøre dyrka eller dyrkbar mark og eller verdifull skog, og skal ikke medføre vesentlige inngrep i LNFR-områder.
+
+Nett- og trafostasjoner kan kun tillates innenfor LNFR-området der plasseringen er teknisk nødvendig og annen plassering ikke er mulig.`,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "17.1.1",
+      tittel: "Tiltak knyttet til eksisterende boligbebyggelse",
+      bestemmelse: `Innenfor boligbebyggelse i LNFR kan det på eksisterende eiendommer med boliger (inkl. garasjer og boder) gjennomføres tiltak som nevnt i pbl. § 20-1 (1) bokstav b, c, e, og f.
+
+Samlet bruksareal (BRA) for hovedbruksenheten og tilhørende garasje skal ikke overstige 250 m2.
+
+Garasje skal ikke overstige 50 m2 BYA og skal plasseres i tilknytning til tun. Garasje skal ikke berøre av dyrket eller dyrkbar mark, inngrep i verdifull skog eller redusere allmennhetens tilgang til utmark.
+
+Det kan tillates kårbolig der dette er godkjent av kommunens landbruksmyndighet. Kårbolig skal ikke overstige 125 m2 BYA.
+
+For bebyggelse gjelder følgende høyder:
+
+Gesimshøyde skal ikke overstige 8,0 meter, og mønehøyde skal ikke overstige 9,0 meter, målt fra gjennomsnittlig planert terreng rundt bygningen.
+
+For garasjer og lignende tiltak gjelder følgende høyder:
+
+a.
+Gesimshøyde skal ikke overstige 3,0 meter og mønehøyde ikke overstige 4,0 meter. Høydene måles fra ferdig planert terrengs gjennomsnitts nivå rundt bygningen.
+
+b.
+Tiltaket skal være underordnet og tilpasset hovedbruksenheten og/eller våningshuset i volum og utforming.`,
+      spesialmerknad: `Føringer for eksisterende boligbebyggelse innenfor LNFR er forholdsvis harmonisert mot LNFR-spredt, og gir rammer for utnyttelse innenfor eksisterende bebyggelse. I stor grad støtter planen seg på lovens krav.`,
+    },
+    {
+      nummer: "17.2",
+      tittel: "LNFR-spredt bolig og fritidsbebyggelse",
+      bestemmelse: `Innenfor LNFR-områder for spredt bolig- og fritidsbebyggelse er oppføring av nye boliger og nye fritidsboliger ikke tillatt.
+
+På eksisterende boligeiendommer kan det tillates tiltak etter pbl. § 20-1 første ledd bokstav b, c, e og f, herunder tilbygg, påbygg, underbygg, garasje og uthus, dersom tiltaket er knyttet til eksisterende lovlig boligbebyggelse.
+
+For eksisterende boligeiendommer gjelder følgende:
+
+a.
+sekundær boenhet kan tillates med bruksareal på maksimalt 60 m2 BRA
+
+b.
+garasje og uthus kan oppføres med samlet bruksareal på inntil 50 m2 BRA
+
+c.
+garasje og uthus skal plasseres innenfor en avstand på maksimalt 20 meter fra eksisterende hovedbolig på samme eiendom.
+
+d.
+samlet bruksareal for boligbebyggelse og tilhørende bygninger skal ikke overstige 200 m2 BRA
+
+e.
+gesimshøyde skal ikke overstige 8,0 meter og mønehøyde skal ikke overstige 9,0 meter
+
+For eksisterende fritidsbebyggelse gjelder følgende:
+
+a.
+samlet bebygd areal skal ikke overstige 125 m² BYA
+
+b.
+gesimshøyde skal ikke overstige 4,0 meter og mønehøyde skal ikke overstige 6,0 meter. Høydene måles fra ferdig planert terrengs gjennomsnitts nivå rundt bygningen.`,
+      spesialmerknad: `Hensikten med bestemmelsen er å gi bærekraftige rammer for eksisterende bebyggelse i LNFR områder som ligger tett opptil boligbebyggelse, som ikke inngår i gårdsdrift, og som ikke er i konflikt med jordbruk eller naturkvaliteter. Hovedforskjellen til LNFR er at LNFR-spredt åpner for sekundær boenhet.`,
+    },
+  ],
 } as const;
