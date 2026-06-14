d06f4f72403e88643b064ba3bbc25de11f12e69d
christianskrede-maker <christianskrede@gmail.com>
2026-06-14 13:02:30 +0200

    Update kapittel15.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel15.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel15.ts
@@ -1,155 +1,232 @@
 export const kapittel15 = {
   deler: [
     {
+      nummer: "15",
+      tittel: "Bestemmelser til landbruks-, natur og friluftsområder (LNFR)",
+      bestemmelse: `
+jf. pbl. § 11-7 nr. 5
+
+Offentlig eller privat vann- og avløpsanlegg kan etableres for å forsyne eksisterende
+hovedbruksenhet. Anlegget skal ikke berøre dyrka eller dyrkbar mark og eller skog, og skal
+ikke medføre vesentlige inngrep i LNFR-områder.
+
+Nett- og trafostasjoner er kun tillatt innenfor LNFR-området der plasseringen er teknisk
+nødvendig og annen plassering ikke er mulig.
+      `,
+      spesialmerknad: ``,
+    },
+    {
       nummer: "15.1",
-      tittel: "Bebyggelse og anlegg (BA)",
-      bestemmelse: `Områder for bebyggelse og anlegg åpner for kombinert nærings- og boligformål.`,
-      spesialmerknad: `Formålet bebyggelse og anlegg benyttes der det, utenfor senterstrukturen/sentrumsformål, åpnes for kombinasjon av nærings- og boligformål. Næringsformålene er inndelt i underformål, i den hensikt å sikre at ulike kategorier næring lokaliseres på hensiktsmessig sted, Jf. kommuneplanens samfunnsdel og prinsippet om «rett virksomhet på rett sted» (ABC-prinsippet).`,
-    },
-    {
-      nummer: "15.1.1",
-      tittel: "Byggeformål BA1",
-      bestemmelse: `Innenfor BA1 skal arealformål begrenses til boligbebyggelse i kombinasjon med ett eller flere av følgende formål; kontor, verksted og lager og bygg- og anleggsvirksomhet. Eksisterende detaljhandelsvirksomhet skal ikke utvides utover eksisterende omfang.
+      tittel: "Fellesbestemmelser LNFR- områder",
+      bestemmelse: `
+pbl. § 11-11 nr. 1
 
-Innenfor bestemmelsesområde xx (Føyka)
-
-Minimum 50% av Føyka-området (regulert delfelt I, II og IV) skal være fri for bebyggelse og tilrettelagt for rekreasjon og fysisk aktivitet.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.1.2",
-      tittel: "Byggeformål BA2",
-      bestemmelse: `Innenfor BA2 skal formål begrenses til boligbebyggelse i kombinasjon med kontor, verksted og lager, bygg og anleggsvirksomhet, samt arealer for plasskrevende varehandel.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.1.3",
-      tittel: "Byggeformål BA3",
-      bestemmelse: `Innenfor BA3 skal formål begrenses til boligbebyggelse i kombinasjon med kontor, verksted og lager, bygg og anleggsvirksomhet.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.1.4",
-      tittel: "Byggeformål BA4",
-      bestemmelse: `Innenfor BA4 skal formål begrenses til bebyggelse for offentlig eller privat tjenesteyting i kombinasjon med boligbebyggelse.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.1.4.1",
-      tittel: "Byggeformål BA4.1 Sosiale boformer (felt SL14, felt ÅN3)",
-      bestemmelse: `Innenfor BA4.1 skal det etableres offentlig eller privat tjenesteyting i kombinasjon med boliger knyttet til sosiale boformer for eldre, eller personer med nedsatt funksjonsevne.
-
-Øvre Hagen gård, felt ÅN3, tillates bygget om til boliger innenfor eksisterende bebyggelse. Gårdstunet skal opprettholdes og anlegget tilpasses kulturlandskapet. Eksisterende terreng og vegetasjon skal inngå som et aktivum i bofelleskapet. Nødvendige tiltak skal iverksettes slik at tilstøtende landbruksarealer skjermes og landbruksdrift kan pågå uforstyrret.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.1.4.2",
-      tittel: "Byggeformål BA4.3 (Tronstad)",
-      bestemmelse: `Innenfor BA4.3 skal det etableres boligbebyggelse i kombinasjon med bebyggelse for offentlig eller privat tjenesteyting, herunder museumsvirksomhet.`,
-      spesialmerknad: ``,
+Tiltak i LNFR-områder skal ikke medføre nedbygging av dyrket eller dyrkbar mark, inngrep i
+skog eller redusere allmennhetens tilgang til utmark.
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen er å sikre jordbruk og ivaretakelse
+av naturverdier i LNF-områder. Naturrestaurerende samt
+natur- og miljøforbedrende tiltak er i utgangpunktet positivt.
+Dette kan også gjelde dreneringstiltak og masseoppfylling
+av rene naturlige masser som forbedrer dyrking, miljø og
+landskapsmessige kvaliteter.
+LNFR-områder skal i hovedsak benyttes til landbruk, naturog
+friluftsformål og skal i utgangspunktet være ubebygd. Det
+gjøres unntak for tiltak knyttet til stedbunden næring og allment
+friluftsliv.
+Kårbolig kan tillates når det er nødvendig for landbruksdriften,
+det kun er én eksisterende bolig på eiendommen, og under
+forutsetning av at kårboligen plasseres som del av gårdstunet.
+Fradeling tillates ikke.
+Gjenoppbygging av bolig kan vurderes dersom plassering,
+volum og høyder i hovedsak opprettholdes, med mulighet
+for justering dersom dette gir bedre landskaps- eller
+strandsonetilpasning.
+Jordlovens bestemmelser om vern av dyrket og dyrkbar mark
+gjelder fram til reguleringsplan er vedtatt, og delvis fram til
+byggetillatelse er gitt.
+Snødeponi kan vurderes regulert innenfor LNFR-områder der
+dette ikke kommer i konflikt med landbruk, vassdrag, natureller
+kulturmiljøverdier.
+      `,
     },
     {
       nummer: "15.2",
-      tittel: "Sentrumsformål",
-      bestemmelse: `I områder avsatt til sentrumsformål skal himlingshøyde i bebyggelsens første etasje langs strøksgater ha høyde minimum 4,5 meter, og arealbruken skal være utadvendt.`,
-      spesialmerknad: `Hensikten med bestemmelsen er å sikre gode og levende sentrumsområder. Gater (kommunal vei og fylkesvei) gjennom sentrumsformål skal primært utformes med gatepreg og inkludere gode løsninger for busstransport, samt separate anlegg for syklister og fotgjengere. Det skal legges særskilt vekt på trafikksikre løsninger for fotgjengere.
+      tittel: "Tiltak knyttet til eksisterende boligeiendommer i LNFR-områder",
+      bestemmelse: `
+pbl. § 11-11 nr. 1 og 2
 
-Sentrumsområder ved kollektivknutepunkt skal utvikles med utgangspunkt i stedets særpreg og i dialog med lokalmiljøet. Fortetting gir ikke i seg selv føringer for volum og høyde, som avklares i reguleringsplan.
+Innenfor boligbebyggelse i LNFR kan det på eksisterende eiendommer med boliger (inkl.
+garasjer og boder) gjennomføres tiltak som nevnt i pbl. §§ 20-4 (a,b.c og e) og 20-5.
 
-Ved regulering skal sentrumsområder bidra til å bygge opp under inkluderende og levende lokalsamfunn, med vekt på sosial struktur, aldersvennlige løsninger og varierte møteplasser for ulike brukergrupper.
+Samlet BYA for hovedbruksenheten og tilhørende garasje skal ikke overstige %-BYA = 15 %
+av tomtens nettoareal, og ikke mer enn 350 m2.
 
-Det skal legges til rette for robuste og fleksible bygg med lang levetid og mulighet for ombruk. Førsteetasjer skal utformes for utadrettede funksjoner og bidra til aktive byrom, samtidig som de sikrer fleksibel arealbruk og framtidsrettede tekniske løsninger.
-
-Asker kommunes arkitekturstrategi skal brukes for å sikre gode kvaliteter.`,
+Det kan tillates kårbolig der dette er godkjent av kommunens landbruksmyndighet. Kårbolig
+skal ikke overstige 125 m2 BYA.
+      `,
+      spesialmerknad: `
+Føringer for eksisterende boligbebyggelse innenfor LNFR er
+forholdsvis harmonisert mot LNFR-spredt, og gir rammer for
+utnyttelse innenfor eksisterende bebyggelse. I stor grad støtter
+planen seg på lovens krav.
+Bestemmelsene skal sikre at eksisterende boligbebyggelse
+i LNFR kan videreutvikles på en kontrollert måte, samtidig
+som hensyn til landbruk, natur, landskap og friluftsliv
+ivaretas. Det legges vekt på å begrense arealbeslag, bevare
+produksjonsgrunnlaget i landbruket og sikre at utviklingen skjer
+i tilknytning til eksisterende tunstruktur.
+Samlet BYA for hovedbruksenheten og tilhørende garasje skal
+ikke overstige %-BYA = 15 % av tomtens nettoareal, og ikke mer
+enn 350 m². Dette har til hensikt å
+• sette en øvre ramme som begrenser bygningsvolum og
+arealbruk i LNFR
+• hindre at eiendommer utvikles til en størrelse som gir karakter
+av regulerte boligfelt
+• sikre at bebyggelsen fortsatt fremstår som underordnet
+landbruks- og landskapsinteressene
+• bidra til å redusere nedbygging av jordbruksarealer og inngrep
+i natur
+Garasje skal ikke overstige 50 m² BYA og skal plasseres i
+tilknytning til tun. Den skal ikke berøre dyrket eller dyrkbar mark,
+verdifull skog eller redusere allmennhetens tilgang til utmark.
+Bestemmelsen har flere formål:
+• Størrelsesbegrensningen sikrer at garasjer forblir underordnet
+hovedboligen og ikke utvikles til større bygg med andre
+funksjoner
+• Krav om plassering i tilknytning til tun samler bebyggelsen og
+reduserer fragmentering av landskapet
+• Vern av dyrket og dyrkbar mark støtter nasjonale og regionale
+jordvernmål og sikrer langsiktig matproduksjon
+• Hensyn til verdifull skog ivaretar naturmangfold, økologiske
+funksjoner og landskapskvaliteter
+• Sikring av allmennhetens tilgang støtter friluftsliv og
+opprettholder tilgjengeligheten til utmark
+Kårbolig (maks 125 m² BYA):
+Det kan tillates kårbolig der dette er godkjent av kommunens
+landbruksmyndighet. Kårbolig skal ikke overstige 125 m² BYA.
+Dette har til hensikt å
+• legge til rette for generasjonsskifte og drift av
+landbrukseiendommer
+• sikre at kårboligen er funksjonelt tilpasset formålet, uten å bli
+en fullverdig, uavhengig bolig
+• sikre at kravet om godkjenning fra landbruksmyndigheten
+sikrer at tiltaket er driftsmessig begrunnet og ikke rent
+boligutvikling
+• hindre unødvendig stort arealbeslag og sikrer underordning til
+gårdsbebyggelsen
+      `,
     },
     {
       nummer: "15.3",
-      tittel: "Fritids- og turistformål (FC og FT)",
-      bestemmelse: ``,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.3.1",
-      tittel: "1FC (Ramton, Solbergstøa, Knattvoldstranda og Rødtangen)",
-      bestemmelse: `Innenfor FC tillates campingplass med tilhørende service.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.3.2",
-      tittel: "FT (Verpen)",
-      bestemmelse: `Innenfor FT tillates utleiehytter og annen turistnæring`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.4",
-      tittel: "Idrettsformål",
-      bestemmelse: ``,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.4.1",
-      tittel: "Avgrunnsdalen",
-      bestemmelse: `Området kan tillates benyttet til skyteanlegg med tilknyttede aktiviteter. Området skal være åpent for allmenheten når anlegget ikke er i aktiv bruk.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.4.2",
-      tittel: "Vardåsen",
-      bestemmelse: `Innenfor området kan det etableres idrettsanlegg under forutsetning av at:
+      tittel: "LNFR-spredt bolig- og fritidsbebyggelse",
+      bestemmelse: `
+pbl. § 11-11 nr. 2
 
-a.
-Eksisterende stier ivaretas i reguleringsplan
+Innenfor LNFR-områder for spredt bolig- og fritidsbebyggelse er oppføring av nye boliger og
+nye fritidsboliger ikke tillatt.
 
-b.
-Det ikke foretas inngrep i Barlindbekken
+På eksisterende boligeiendommer kan det tillates tiltak etter pbl. §§ 20-4(a,b.c og e) og 20-5,
+inkludert tilbygg, påbygg, underbygg, garasje og uthus, dersom tiltaket er knyttet til
+eksisterende boligbebyggelse.
 
-c.
-Kartlagte naturtyper i området sikres mot inngrep gjennom reguleringsplan`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.5",
-      tittel: "Råstoffutvinning",
-      bestemmelse: `Områder som er avsatt tilråstoffutvinning tillates ikke bebygget eller benyttet på en måte som vanskeliggjør framtidigutvinning av mineralske ressurser.`,
-      spesialmerknad: `Hensikten er å sikre mulig fremtidig uttak av mineralske ressurser.`,
-    },
-    {
-      nummer: "15.6",
-      tittel: "Andre typer anlegg (ATA)",
-      bestemmelse: ``,
-      spesialmerknad: `Hensikten med bestemmelsene er å sikre arealer for masseforvaltning.
+For eksisterende boligeiendommer gjelder følgende:
 
-Ved regulering av områder for masseforvaltning bør det vurderes å avsette arealer til snødeponi. Dette kan særlig være aktuelt innenfor områder avsatt til massehåndtering (f.eks. ATA1, ATA1.1, ATA2 og NB2), da disse ofte har avklarte forhold knyttet til avrenning, støy og trafikk.
+a. sekundær boenhet kan tillates med bruksareal på maksimalt 60 m2 BRA, med
+tilhørende garasje
 
-Etablering av snødeponi forutsetter at reguleringsplan åpner for slik bruk, samt tillatelse fra forurensningsmyndighet. Ved regulering skal det vurderes avbøtende tiltak knyttet til støy, forurensning, håndtering av smeltevann og trafikk. Det skal også vurderes om tiltaket medfører økt støy eller trafikk som utløser behov for ny regulering.`,
-    },
-    {
-      nummer: "15.6.1",
-      tittel: "Byggeformål ATA.1",
-      bestemmelse: `Innenfor ATA1 tillates uttak og mottak av masser, deponi, samt arealer for mellomlagring og gjenbruk av masser. Mottak og deponi av masser, og eventuelle bygningsmessige tiltak, skal ikke vanskeliggjøre framtidig utvinning av mineralske ressurser innenfor området.
+b. garasje og uthus skal plasseres innenfor en avstand på maksimalt 20 meter fra
+boligens yttervegg på samme eiendom
 
-• Byggeformål ATA1.2 Follestad: Området kan kombineres med idrettsformål.
+c. tiltaket skal være underordnet og tilpasset hovedbruksenheten i volum og utforming
 
-• Byggeformål ATA1.3 Stokkeråsen: Innenfor ATA1.3 kan det deponeres snø, under forutsetning av at området reguleres.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.6.2",
-      tittel: "Byggeformål ATA2",
-      bestemmelse: `Innenfor ATA2 kan arealbruk begrenses til mottak av masser, deponi, samt arealer for mellomlagring og gjenbruk av masser.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.6.3",
-      tittel: "Byggeformål ATA3",
-      bestemmelse: `ATA3 er avsatt til areal for transformatorstasjoner.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "15.6.4",
-      tittel: "Byggeformål ATA4",
-      bestemmelse: `ATA4 er avsatt til areal for infrastrukturanlegg.`,
-      spesialmerknad: ``,
+d. samlet bruksareal for boligbebyggelse og tilhørende bygninger skal ikke overstige %-
+BYA = 25 % av tomtens nettoareal, og ikke mer enn 250 m2 BYA
+
+For eksisterende fritidsbebyggelse gjelder følgende:
+
+a. samlet bebygd areal skal ikke overstige 125 m² BYA
+
+b. gesimshøyde skal ikke overstige 4,0 meter og mønehøyde skal ikke overstige 6,0
+meter. Høydene måles fra ferdig planert terrengs gjennomsnitts nivå rundt bygningen
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen er å gi bærekraftige rammer for
+eksisterende bebyggelse i LNFR områder som ligger tett opptil
+boligbebyggelse, som ikke inngår i gårdsdrift, og som ikke er i
+konflikt med jordbruk eller naturkvaliteter. Hovedforskjellen til
+LNFR er at LNFR-spredt åpner for sekundær boenhet.
+Bestemmelsene skal sikre at LNFR-spredt fortsatt ivaretar
+sine grunnleggende funksjoner knyttet til landbruksdrift,
+naturmangfold, landskap og friluftsinteresser. Samtidig
+åpnes det for en begrenset videreutvikling av eksisterende
+boligbebyggelse på en måte som ikke svekker disse verdiene
+eller bidrar til uønsket spredt utbygging.
+Bestemmelsene balanserer hensynet til vern og bærekraftig
+arealbruk med behovet for fleksibilitet for eksisterende
+eiendommer. De bidrar til å
+• bevare LNFR-områdenes funksjon og karakter
+• hindre uønsket spredt utbygging
+• sikre effektiv arealbruk og redusert klimaavtrykk
+• gi klare og forutsigbare rammer for tiltakshavere og forvaltning
+Innenfor LNFR-områder for spredt bolig- og fritidsbebyggelse
+er oppføring av nye boliger og nye fritidsboliger ikke tillatt.
+Begrunnelsen er å
+• hindre ytterligere spredt utbygging i områder som primært er
+avsatt til landbruk, natur og friluftsliv
+• redusere press på arealer som er viktige for matproduksjon og
+naturmangfold
+• bidra til en mer bærekraftig arealutvikling ved at boligvekst
+i større grad kanaliseres til avsatte byggeområder med
+eksisterende infrastruktur og kollektivtilbud
+• forebygge økte kostnader til teknisk infrastruktur og
+tjenesteyting (vei, vann, avløp, skoletransport mv.)
+På eksisterende boligeiendommer kan det tillates tiltak etter
+pbl. §§ 20-4 (a, b, c og e) og 20-5, dersom tiltaket er knyttet til
+eksisterende boligbebyggelse. Med tiltak etter pbl. §§ 20 4 (a,
+b, c og e) og 20 5 menes mindre tiltak på eksisterende bebygd
+eiendom, herunder tilbygg, påbygg, underbygg, fasadeendringer
+samt oppføring av mindre frittliggende bygg som garasje og
+uthus. Bestemmelsen omfatter både søknadspliktige tiltak som
+kan forestås av tiltakshaver og mindre tiltak som er unntatt
+søknadsplikt etter gjeldende regelverk. Dette bidrar til bedre
+utnyttelse av eksisterende arealer fremfor nybygging på nye
+tomter. Videre vil dette gi mulighet for tilpasning til endrede
+husholdningsbehov, f.eks. generasjonsbolig eller utleiedel.
+Maksimalt %-BYA = 25 % av tomtens nettoareal, og ikke mer
+enn 250 m² BYA, hindrer gradvis omforming til mer tettbygd
+karakter. I tillegg vil det sikre at eiendommene fortsatt fremstår
+som del av LNFR-område og ikke boligfelt.
+Sekundær boenhet på maksimalt 60 m² BRA begrunnes med at
+den
+• gir mulighet for tilpasning til endrede husholdningsbehov,
+f.eks. generasjonsbolig eller utleiedel
+• bidrar til boligtilbud uten å legge til rette for nye selvstendige
+boligtomter
+• størrelsesbegrensningen sikrer at boenheten forblir
+underordnet hovedboligen og begrenser økt trafikk og
+belastning på infrastrukturen
+Høydebegrensninger (8,0 m gesims / 9,0 m møne) følger de
+generelle bestemmelsene i plan- og bygningsloven og ivaretar
+landskaps- og fjernvirkning ved å begrense dominerende
+bygningsvolum. Den sikrer tilpasning til eksisterende
+bebyggelsesstruktur og terreng, og reduserer visuell påvirkning i
+åpne kulturlandskap og viktige landskapsrom.
+Plassering innen 20 meter fra boligen har til hensikt å
+• samle bebyggelsen og reduserer arealbeslag og fragmentering
+av landskapet
+• begrense inngrep i jordbruks- og naturområder
+• opprettholde en tydelig tunstruktur og bedre
+landskapstilpasning
+Arealberegningen på garasje og uthus (maks samlet 50 m² BRA)
+har til hensikt å
+• dekke nødvendige funksjoner knyttet til boligbruk, lagring og
+parkering
+• hindre etablering av bygg som kan få karakter av selvstendige
+bruksenheter eller næringsformål
+• bidra til å opprettholde områdets landlige og åpne preg
+      `,
     },
   ],
 } as const;
