25fe17a4c34027d9068f9e528c4c911f9ca0f86e
christianskrede-maker <christianskrede@gmail.com>
2026-06-14 13:26:02 +0200

    Update kapittel21.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel21.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel21.ts
@@ -2,9 +2,69 @@ export const kapittel21 = {
   deler: [
     {
       nummer: "21",
-      tittel: "Hensynssone infrastruktur – flomveier",
-      bestemmelse: "Naturlige flomveier skal ivaretas innenfor hensynssonen. Flomveier skal utformes slik at vannet transporteres bort uten å gjøre skade nedstrøms. Det skal sikres trygg avrenning fra byggeområder til naturlige flomveier. Tørrbekker skal holdes åpne og fungere som flomveier. Bygninger og anlegg ved flomveier skal utformes slik at naturlige flomveier ivaretas.\n\nDet skal avsettes areal for nye flomveier ved planlegging og søknad om tiltak som berører eksisterende flomveier. Ved etablering av flomveier skal omkringliggende arealer, som bygninger og annen infrastruktur, sikres mot flomskader. Flomveier, drenslinjer, flomsoner, erosjon og stormflo fremgår av aktsomhetskart i kommunes kartløsning og skal legges til grunn i plan- og byggesaker.",
-      spesialmerknad: "Hensikten med hensynssonen er å sikre naturlige flomveier for å redusere risikoen for uønskede hendelser knyttet til flom.\n\nDet må redegjøres for flomveier i reguleringsplanen og sikre avbøtende tiltak i planen. Flomveier og drenslinjer vurderes spesielt ved bratt terreng.",
+      tittel: "Båndleggingssoner",
+      bestemmelse: ``,
+      spesialmerknad: `
+Hensikten med båndleggingssonene er å vise områder
+som er båndlagt etter andre lover, som kulturminneloven,
+naturmangfoldloven og energiloven.
+      `,
+    },
+    {
+      nummer: "21.1",
+      tittel: "Båndlagt areal etter lov om kulturminner, H730",
+      bestemmelse: `
+Jf. pbl. § 11-8, bokstav d
+
+Hensynssonen viser områder som er båndlagt etter kulturminneloven, inkludert automatisk
+fredete kulturminner, fredete kulturmiljøer og områder omfattet av fredningsvedtak. Innenfor
+hensynssonen gjelder kulturminnelovens bestemmelser og eventuelle fredningsvedtak.
+      `,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "21.1.1",
+      tittel: "Automatisk fredet kirke og -gravplass etter kulturminneloven",
+      bestemmelse: `
+Det er ikke tillatt å iverksette tiltak som kan virke inn på den automatisk fredet kirke og
+gravplass med mindre det foreligger dispensasjon fra kulturminneloven. Innenfor
+middelaldergravplass er gravlegging kun tillatt i graver som har vært i kontinuerlig bruk etter
+1945. Graver som ikke har vært i bruk etter 1945 skal ikke benyttes til gravlegging. Alle tiltak
+innenfor kirkestedet skal vurderes av kulturmiljøforvaltningen opp mot bestemmelsene i
+kulturminneloven.
+      `,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "21.2",
+      tittel: "Båndlagt etter naturmangfoldloven, H720",
+      bestemmelse: `
+Jf. pbl. § 11-8, bokstav d
+
+Forskrifter vedtatt i medhold av naturmangfoldloven eller naturvernloven gjelder.
+      `,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "21.3",
+      tittel: "Båndlagt areal etter markaloven, H740_3",
+      bestemmelse: `
+Jf. pbl. § 11-8, bokstav d
+
+Innenfor markagrensen vist på kommuneplanens arealplankart gjelder «Lov om
+naturområder i Oslo og nærliggende kommuner»
+      `,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "21.4",
+      tittel: "Båndlagt areal etter Energiloven, H740_1-2",
+      bestemmelse: `
+Jf. pbl. § 11-8, bokstav d
+
+Det er ikke tillatt med ny bebyggelse innenfor hensynssonen
+      `,
+      spesialmerknad: ``,
     },
   ],
 } as const;
