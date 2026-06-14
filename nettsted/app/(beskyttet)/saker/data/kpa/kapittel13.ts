5cdefbf77d0ca66948e0c65c0060a125a5870caa
christianskrede-maker <christianskrede@gmail.com>
2026-06-14 12:54:13 +0200

    Update kapittel13.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel13.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel13.ts
@@ -1,40 +1,315 @@
 export const kapittel13 = {
   deler: [
     {
+      nummer: "13",
+      tittel: "Bestemmelser til andre arealformål under bebyggelse og anlegg",
+      bestemmelse: `
+jf. pbl. § 11-7 nr. 1
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsene er å sikre helhetlig arealutvikling
+i tråd med arealstrategien.
+Anbefalt fordeling av boligstørrelse ved feltutbygging:
+Ved bygging av felt med mer enn 50 boliger gjelder følgende
+boligfordeling:
+a. minimum 20 % av alle boliger bør være mellom 30 – 50 m2
+BRA
+b. minimum 20 % av alle boliger bør være over 80 m2 BRA.
+Minimum 50 % av disse boligene bør ha utgang på bakkeplan.
+For bofellesskap med åtte eller flere boenheter kan det
+tillates boliger ned til 23 m², forutsatt at det etableres felles
+innendørsareal tilsvarende minst 10 % av samlet boareal, og
+minimum 5 m² per boenhet.
+      `,
+    },
+    {
       nummer: "13.1",
-      tittel: "Fritidsbebyggelse utenfor 100-metersbeltet langs sjø",
-      bestemmelse: `Det skal kun etableres én fritidsbolig på eksisterende ubebygget tomt og utvidelse av eksisterende fritidsbebyggelse i områder avsatt til fritidsbebyggelse eller kombinert bolig- og fritidsbebyggelse felt KBF, under følgende forutsetninger:
+      tittel: "Bebyggelse og anlegg (BA)",
+      bestemmelse: `
+jf. pbl. § 11-7 nr. 1
 
-a.
-Tilknytning til vann og avløp. Fritidsboligen skal tilknyttes godkjent vann- og avløpsanlegg.
+Følgende områder for bebyggelse og anlegg åpner for kombinert nærings- og boligformål:
 
-b.
-Utvidelse av eksisterende fritidsbolig kan ikke medføre opprettelse av ny boenhet på tomten. Grad av utnytting skal ikke overskride %-BYA = 15 % av tomtens nettoareal, og samlet BYA skal overskrider ikke 125 m².
+• BA1: Innenfor BA1 skal arealformål begrenses til boligbebyggelse i kombinasjon med
+ett eller flere av følgende formål; kontor, verksted og lager og bygg- og
+anleggsvirksomhet. Eksisterende detaljhandelsvirksomhet skal ikke utvides utover
+eksisterende omfang.
 
-c.
-Høyder. Gesimshøyde tillates inntil 4,0 meter og mønehøyde tillates inntil 6,0 meter, målt fra ferdig planert terrengs gjennomsnittsnivå rundt bygningen. Gesimshøyden skal likevel ikke på noe punkt overstige 5 m fra laveste punkt på ferdig planert terreng.`,
-      spesialmerknad: `Hensikten med bestemmelsen er å primært å etablere helhetlige rammer for individuell utbygging av fritidsbebyggelse.
+• Bestemmelsesområde #5 (Føyka) innenfor byggeformål BA1:
+Minimum 50% av Føyka-området (regulert delfelt I, II og IV) skal være fri for
+bebyggelse og tilrettelagt for rekreasjon og fysisk aktivitet.
 
-Bruksendring fra fritidsbolig til bolig er spesielt aktuelt i KBF områdene, fire konkrete områdene, Brønnøya, Storsand, Holmsbu og Torgrimsåsen hvor det allerede er boligbebyggelse. Der ny bruk vil være i strid med regulert arealformål, kreves ny reguleringsplan eller endring av gjeldende plan.`,
+• BA2: Innenfor BA2 skal formål begrenses til boligbebyggelse i kombinasjon med
+kontor, verksted og lager, bygg og anleggsvirksomhet, og arealer for plasskrevende
+varehandel.
+
+• BA3: Innenfor BA3 skal formål begrenses til boligbebyggelse i kombinasjon med
+kontor, verksted og lager, bygg og anleggsvirksomhet.
+
+• BA4: Innenfor BA4 skal formål begrenses til bebyggelse for offentlig eller privat
+tjenesteyting i kombinasjon med boligbebyggelse.
+
+• BA4.1 Sosiale boformer (felt SL14, felt ÅN3)
+
+• Innenfor BA4.1 skal det etableres offentlig eller privat tjenesteyting i kombinasjon
+med boliger knyttet til sosiale boformer for eldre, eller personer med nedsatt
+funksjonsevne.
+
+• Øvre Hagen gård, felt ÅN3, er tillatt bygget om til boliger innenfor eksisterende
+bebyggelse. Gårdstunet skal opprettholdes og anlegget tilpasses kulturlandskapet.
+Eksisterende terreng og vegetasjon skal inngå som et aktivum i bofelleskapet.
+Nødvendige tiltak skal iverksettes slik at tilstøtende landbruksarealer skjermes og
+landbruksdrift kan pågå uforstyrret.
+
+• BA4.3 (Tronstad): Innenfor BA4.3 skal det etableres boligbebyggelse i kombinasjon
+med bebyggelse for offentlig eller privat tjenesteyting, inkludert museumsvirksomhet.
+      `,
+      spesialmerknad: `
+Formålet Bebyggelse og anlegg benyttes der det, utenfor
+senterstrukturen/sentrumsformål, åpnes for kombinasjon av
+nærings- og boligformål. Dette gjelder transformasjonsområder
+eller feltutbygging som inngår i boligbyggeprogrammet.
+Utbygging forutsetter reguleringsplan.
+Ved samlet utbygging over fire boenheter skal behovet for
+fellesskapsarealer, deleløsninger og sosiale boformer vurderes
+for å sikre gode bokvaliteter og nabolag. Hensikten med
+fellesskapsarealer er å legge til rette for sosial interaksjon og
+fellesskap, tilleggsfunksjoner utover egen boenhet og å øke
+attraktiviteten til prosjektet. Kvaliteten på fellesskapsarealene
+bør vektlegges, og arealene bør tilpasses målgruppene for
+prosjektet.
+Fellesskapsarealene bør legge til rette for sambruk og
+deleløsninger, for eksempel ved å ha funksjoner som felles
+gjesterom, verksted, vaskerom, eller felles oppholdsareal som
+kan benyttes til selskaper og når alle beboerne skal samles.
+      `,
     },
     {
       nummer: "13.2",
-      tittel: "Fritidsbebyggelse beliggende i 100-metersbeltet, utenfor byggegrense mot sjø",
-      bestemmelse: `Det tillates ett tilbygg på inntil 8 m² til eksisterende fritidsbebyggelse i områder avsatt til kombinert bolig- og fritidsbebyggelse (KBF) og fritidsbebyggelse beliggende i 100-metersbeltet, utenfor byggegrense mot sjø, når følgende vilkår er oppfylt:
+      tittel: "Sentrumsformål",
+      bestemmelse: `
+jf. pbl. § 11-7 nr. 1
 
-1.
-tilbygget er nødvendig for å sikre avkloakkering av eiendommen ved tilkobling til godkjent vann- og avløpsanlegg,
+I områder avsatt til sentrumsformål skal himlingshøyde i bebyggelsens første etasje langs
+strøksgater ha høyde minimum 4,5 meter, og arealbruken skal være utadvendt.
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen er å legge til rette for
+gode, inkluderende og levende sentrumsområder med
+funksjonsblanding, urbane kvaliteter og tydelig stedlig identitet.
+Sentrumsformål er et fleksibelt arealformål som kan omfatte
+blant annet bolig, nye boformer, detaljhandel, service, kultur,
+bevertning, kontor, tjenesteyting, møteplasser, grønnstruktur og
+nødvendig teknisk og sosial infrastruktur.
+Bestemmelsen skal bidra til at sentrumsområdene utvikles
+som attraktive lokalsamfunn med varierte møteplasser for
+ulike grupper av befolkningen. Utvikling og fortetting skal
+ta utgangspunkt i stedets særpreg, landskap, kulturmiljø,
+eksisterende bebyggelsesstruktur og lokalmiljø. Fortetting
+innenfor sentrumsformål gir ikke i seg selv føringer for volum
+og høyde; dette må avklares i reguleringsplan ut fra stedets
+tåleevne, ønsket sentrumsstruktur, byromskvaliteter og hensynet
+til omgivelsene.
+Gater gjennom sentrumsområder bør utvikles med
+gatepreg og som trygge, attraktive forbindelser for gående, syklende og kollektivreisende. Det bør legges særlig vekt
+på trafikksikre løsninger for fotgjengere, gode løsninger for
+busstransport og separate anlegg for syklister og fotgjengere
+der dette er nødvendig. I lokal- og nærsentre bør behovet
+for mobilitetspunkt vurderes, herunder løsninger for felles
+sykkelparkering, delingsmobilitet, kollektivinformasjon,
+pakkehenting, ladepunkter og sykkelservice.
+Førsteetasjer mot sentrale gater, torg og byrom bør utformes
+slik at de gir aktive, trygge og inviterende byrom, samtidig som
+byggene får fleksible lokaler og tekniske løsninger som kan
+tilpasses endrede behov over tid. Det bør legges til rette for
+robuste bygg med lang levetid, mulighet for ombruk og god
+arkitektonisk kvalitet. Asker kommunes arkitekturstrategi
+skal brukes som grunnlag for vurdering av stedstilpasning,
+byromskvalitet, materialbruk, skala, førsteetasjer, møteplasser
+og helhetlig sentrumsutvikling.
+      `,
+    },
+    {
+      nummer: "13.3",
+      tittel: "Næringsformål",
+      bestemmelse: `
+jf. pbl. § 11-7 nr. 1
 
-2.
-det kan dokumenteres at sanitæranlegg ikke kan integreres i eksisterende bebyggelse,
+Arealer skal benyttes til næring. Reguleringsplan avklarer kombinasjonsformål.
 
-1.
-tilbygget plasseres i tilknytning til hovedbruksenheten, i retning bort fra sjø.`,
-      spesialmerknad: `Hensikten med bestemmelsen er ment som en snever unntaksbestemmelse for å kunne etablere nødvendige sanitæranlegg i eksisterende fritidsbebyggelse hvor dette ikke lar seg løse innenfor eksisterende bygningsmasse. Vurderingen av nødvendighet må foretas konkret i den enkelte sak.
+• NÆ1: arealformål skal begrenses til næring og forretning.
 
-Det er søker/tiltakshaver som må fremme en begrunnet søknad og redegjøre for hvorfor det er nødvendig med den prosjekterte størrelsen på tilbygget. At bestemmelsen åpner for tilbygg på inntil 8 m² innebærer ikke at det automatisk anses nødvendig med et tilbygg av denne størrelsen. Tiltaket skal begrenses til det arealet som er nødvendig for å etablere sanitæranlegget på en funksjonell og forsvarlig måte.
+• NÆ2: arealformål skal begrenses til næring og offentlig eller privat tjenesteyting.
+Detaljhandel er ikke tillatt.
 
-Tilbygget skal kun benyttes til sanitærfunksjoner knyttet til bad/wc. Det tillates ikke at arealet benyttes til gangareal, oppholdsrom, bod eller andre formål utover det som er nødvendig for sanitæranlegget.`,
+• NÆ3: arealformål skal begrenses til næring.
+
+• NÆ4: arealformål skal begrenses til næring.
+
+• NÆ5: arealformål skal benyttes til Kystkultursenter.
+
+• NÆ6: arealformål kan benyttes til industrivirksomheter.
+
+• NÆ7: arealformål skal benyttes til virksomheter som legger til rette for reiseliv, som
+hotell, servering og annen næringsvirksomhet.
+
+• NÆ8: arealformål benyttes til industrielle aktiviteter, aktiviteter for bevaring og
+formidling av områdets kulturelle og industrielle historie, kontorer og andre egnede
+næringsaktiviteter i sammenheng med dette.
+      `,
+      spesialmerknad: `
+Hovedformålet for arealene er næring. Hensikten bak de ulike
+kategoriene er å tilrettelegge for næringsvirksomhet ut fra ABCprinsippet,
+med riktig næring på riktig sted. Ved regulering
+åpnes det derfor for en kombinasjon av arealformål som angitt
+under de ulike kategoriene.
+Formålet næringsbebyggelse benyttes der det,
+utenfor senterstrukturen/sentrumsformål, åpnes for
+næringsvirksomhet. Næringsformålene er inndelt i underformål,
+i den hensikt å sikre at ulike kategorier næring lokaliseres
+på hensiktsmessig sted, Jf. kommuneplanens samfunnsdel
+og prinsippet om «rett virksomhet på rett sted». Formålet
+er derfor inndelt i seks underformål, som angir ulike type
+næringsvirksomhet som er aktuelle. Kombinasjon av disse
+typene næringsvirksomhet må vurderes særskilt og avklares i
+reguleringsprosess.
+NÆ 1
+Hensikt med bestemmelsen er å etablere arbeidsintensive
+virksomheter med mange ansatte og besøkende, og tilrettelegge
+for blant annet detaljhandel og større kontorvirksomheter.
+Innenfor kategori A i abc-prinsippet.
+NÆ2
+Hensikt med bestemmelsen er å etablere arbeidsintensive
+virksomheter med mange ansatte og besøkende, blant annet
+større kontorvirksomheter, og å åpne opp for å etablere
+bebyggelse for offentlig eller privat tjenesteyting.
+Innenfor kategori A i abc-prinsippet.
+NÆ3
+Hensikten med bestemmelsen er å etablere transportbedrifter,
+verksteder, lagerhaller, bygg- og anleggsvirksomheter, ikkestøyende
+industrivirksomheter og plasskrevende varehandel.
+Innenfor kategori B/C i abc-prinsippet.
+NÆ4
+Hensikten med bestemmelsen er å etablere verksteder,
+transportbedrifter, lagerhaller og samleterminal, bygg- og
+anleggsvirksomheter, og ikke-støyende industrivirksomheter.
+Innenfor kategori B/C i abc-prinsippet.
+NÆ5
+Tirlettelegge for kystkultursenter.
+NÆ6
+Hensikten med bestemmelsen er å etablere industrivirksomhet.
+Innenfor kategori C i abc-prinsippet.
+NÆ7
+Hensikten med bestemmelsen er å skape forutsigbarhet og
+stimulere til etablering av virksomheter som legger til rette for
+reiseliv, som hotell, servering og annen næringsvirksomhet.
+NÆ8
+Hensikten med bestemmelsen er å tilrettelegge for og stimulere
+til etablering av industrielle aktiviteter, aktiviteter for bevaring
+og formidling av områdets kulturelle og industrielle historie,
+kontorer og andre egnede næringsaktiviteter i sammenheng
+med dette.
+      `,
+    },
+    {
+      nummer: "13.4",
+      tittel: "Fritids- og turistformål (FC og FT)",
+      bestemmelse: `
+Jf. pbl. §§ 11-7, nr. 1 og 11-9. nr. 5
+
+FC områder skal benyttes til campingplass med tilhørende service.
+FT områder skal benyttes til utleiehytter og annen turistnæring.
+      `,
+      spesialmerknad: `
+Hensikten er å tilrettelegge for turisme og næring.
+      `,
+    },
+    {
+      nummer: "13.5",
+      tittel: "Idrettsformål",
+      bestemmelse: `
+Jf. pbl. §§ 11-7, nr. 1 og 11-9. nr. 3, 5 og 6
+      `,
+      spesialmerknad: `
+Hensikten med formålet er å tilrettelegge for idrett og tilknyttede
+aktiviteter.
+      `,
+    },
+    {
+      nummer: "13.5.1",
+      tittel: "Avgrunnsdalen",
+      bestemmelse: `
+Området skal benyttes til skyteanlegg med tilknyttede aktiviteter. Området skal være åpent
+for allmenheten når anlegget ikke er i aktiv bruk.
+      `,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "13.5.2",
+      tittel: "Vardåsen",
+      bestemmelse: `
+Området skal benyttes til idrettsanlegg under forutsetning av at
+
+a. eksisterende stier ivaretas i reguleringsplan
+
+b. det ikke foretas inngrep i Barlindbekken
+
+c. kartlagte naturtyper i området sikres mot inngrep gjennom reguleringsplan
+      `,
+      spesialmerknad: ``,
+    },
+    {
+      nummer: "13.6",
+      tittel: "Råstoffutvinning",
+      bestemmelse: `
+Jf. pbl. §§ 11-7, nr. 1 og 11-9. nr. 3, 5 og 6
+
+Områder som er avsatt tilråstoffutvinning er ikke tillatt bebygget eller benyttet på en måte
+som vanskeliggjør framtidigutvinning av mineralske ressurser.
+      `,
+      spesialmerknad: `
+Hensikten med formålet er å sikre mulig fremtidig uttak av
+mineralske ressurser.
+      `,
+    },
+    {
+      nummer: "13.7",
+      tittel: "Andre typer anlegg (ABA)",
+      bestemmelse: `
+Jf. Pbl. §§ 11-7, nr. 1 og 11-9. nr. 5
+
+ABA1 skal benyttes til uttak og mottak av masser, deponi, og arealer for mellomlagring og
+gjenbruk av masser. Mottak og deponi av masser, og eventuelle bygningsmessige tiltak, skal
+ikke vanskeliggjøre framtidig utvinning av mineralske ressurser innenfor området.
+
+ABA1.2 Follestad:
+Området kan kombineres med idrettsformål.
+
+ABA1.3 Stokkeråsen:
+Innenfor ABA1.3 kan det deponeres snø. Avklares i reguleringsplan.
+
+Innenfor ABA2 skal arealbruk begrenses til mottak av masser, deponi, og arealer for
+mellomlagring og gjenbruk av masser.
+
+ABA3 er avsatt til areal for transformatorstasjoner.
+
+ABA4 er avsatt til areal for infrastrukturanlegg.
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsene er å sikre arealer for
+masseforvaltning, transformatorstasjoner og annen
+infrastrukturanlegg.
+Ved regulering av områder for masseforvaltning bør det
+vurderes å avsette arealer til snødeponi. Dette kan særlig være
+aktuelt innenfor områder avsatt til massehåndtering (f.eks.
+ABA1, ABA1.1, ABA2 og NÆ4), da disse ofte har avklarte forhold
+knyttet til avrenning, støy og trafikk.
+Etablering av snødeponi forutsetter at reguleringsplan åpner
+for slik bruk, samt tillatelse fra forurensningsmyndighet. Ved
+regulering skal det vurderes avbøtende tiltak knyttet til støy,
+forurensning, håndtering av smeltevann og trafikk. Det skal også vurderes om tiltaket medfører økt støy eller trafikk som utløser
+behov for ny regulering.
+      `,
     },
   ],
 } as const;
