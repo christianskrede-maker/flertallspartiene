22b9454176da4e69ed0e0e8d17ce30d795393ab9
christianskrede-maker <christianskrede@gmail.com>
2026-06-14 12:34:00 +0200

    Update kapittel9.ts

--- nettsted/app/(beskyttet)/saker/data/kpa/kapittel9.ts
+++ nettsted/app/(beskyttet)/saker/data/kpa/kapittel9.ts
@@ -3,83 +3,62 @@ export const kapittel9 = {
     {
       nummer: "9.1",
       tittel: "Returpunkt, pakkeautomater etc.",
-      bestemmelse: `Returpunkt, pakkeautomater og tilsvarende mindre tekniske servicefunksjoner kan plasseres innenfor arealformålene bebyggelse og anlegg, samferdselsanlegg og teknisk infrastruktur dersom:
+      bestemmelse: `
+Jf. pbl. § 11-9, nr. 5 og 11-10 nr. 1
 
-a.
-tiltaket ikke hindrer trafikkavvikling eller framkommelighet for gående og syklende
+Returpunkt, pakkeautomater og tilsvarende mindre tekniske servicefunksjoner er, innenfor
+arealformålene 11-7 nr. 1 og 2, tillatt på følgende vilkår:
 
-b.
-tiltaket ikke kommer i konflikt med vann- og avløpsledninger, flomveier, frisikt, byggegrenser, veivedlikehold, turstier eller allmenn ferdsel
+a. tiltaket ikke hindrer trafikkavvikling eller framkommelighet for gående og syklende
 
-c.
-tiltaket ikke beslaglegger nødvendig parkeringsareal
+b. tiltaket ikke kommer i konflikt med vann- og avløpsledninger, flomveier, frisikt,
+byggegrenser, veivedlikehold, turstier eller allmenn ferdsel
 
-d.
-det er sikret forsvarlig adkomst og korttidsparkering for henting og levering`,
-      spesialmerknad: ``,
+c. tiltaket ikke beslaglegger nødvendig parkeringsareal
+
+d. det er sikret forsvarlig adkomst og korttidsparkering for henting og levering
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen er å sikre nødvendig etablering
+av leveranse- og returpunkt, samt å gi rammer for den konkrete
+vurderingen av hvor slike punkter bør etableres.
+      `,
     },
     {
       nummer: "9.2",
       tittel: "Nettstasjoner",
-      bestemmelse: `Nettstasjoner kan plasseres utenfor regulert byggegrense og inntil 2 meter fra eiendomsgrense og regulert vei, dersom tiltaket ikke kommer i konflikt med vann- og avløpsledninger, flomveier, frisikt, veivedlikehold, turstier eller allmenn ferdsel.
+      bestemmelse: `
+Jf. pbl. § 11-9, nr. 5
 
-Nettstasjoner skal ha minst 5 meter avstand til annen bebyggelse.`,
-      spesialmerknad: `Hensikten med bestemmelsen er å sikre nødvendig etablering av nettstasjoner. Disse skal fortrinnsvis plasseres innenfor arealformålene bebyggelse og anlegg, samferdselsanlegg eller teknisk infrastruktur.`,
+Nettstasjoner kan plasseres utenfor regulert byggegrense og inntil 2 meter fra
+eiendomsgrense og regulert vei, dersom tiltaket ikke kommer i konflikt med vann- og
+avløpsledninger, flomveier, frisikt, veivedlikehold, turstier eller allmenn ferdsel.
+
+Nettstasjoner skal ha minst 5 meter avstand til annen bebyggelse.
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen er å sikre nødvendig etablering
+av nettstasjoner. Disse skal fortrinnsvis plasseres innenfor
+arealformålene bebyggelse og anlegg, samferdselsanlegg eller
+teknisk infrastruktur.
+      `,
     },
     {
       nummer: "9.3",
       tittel: "Kabler og ledningsanlegg",
-      bestemmelse: `Nye lednings-, kabel- og fiberanlegg, og utskifting av eksisterende anlegg, skal legges i bakken. Kravet gjelder ikke i LNFR-områder dersom terrenginngrep eller naturinngrep tilsier at luftstrekk samlet sett er mindre konfliktfylt.
+      bestemmelse: `
+jf. pbl. § 11-9, nr. 3 og nr. 6
 
-Alléer, verdifulle trær og vegetasjon skal bevares.`,
-      spesialmerknad: ``,
-    },
-    {
-      nummer: "9.4",
-      tittel: "Brygger som er unntatt fra byggegrense mot sjø",
-      bestemmelse: `Oppføring av nye brygger, forutsetter at følgende krav er oppfylt:
+Nye lednings-, kabel- og fiberanlegg, og utskifting av eksisterende anlegg, skal legges i
+bakken. Kravet gjelder ikke i LNFR-områder dersom terrenginngrep eller naturinngrep tilsier
+at luftstrekk samlet sett er mindre konfliktfylt.
 
-a.
-Området omfattes av reguleringsplan vedtatt etter 11.09.1993 som åpner for oppføring av brygger.
-
-b.
-Ikke mer enn én fastbrygge på maks 10 m² per eiendom med strandlinje.
-
-c.
-Fastbrygger som oppføres langsetter sjøkanten skal ikke ta opp mer enn 6 meter av strandlinjen.
-
-d.
-Fastbrygger skal bygges som pelebrygge eller utkraget brygge uten utriggere.
-
-e.
-Der hvor det er langgrunt skal flytebrygge maks gå ut til sjødybde på 1,5 meter middelvannstand. Lengden på flytebryggen skal uansett ikke overstige 10 meter inkludert landgang, og bredden skal være maksimalt 2,4 meter.
-
-f.
-Bryggen skal ikke ha rekkverk/levegger, lyssetting, flaggstenger eller andre privatiserende elementer.
-
-g.
-Oppføringen innebærer ikke sprengningsarbeider, støping i sjøbunn i form av tett konstruksjon/bølgebryter.
-
-Tiltak på etablerte brygger
-
-Ved riving og/eller gjenoppføring og andre tiltak på lovlig etablert brygge gjelder følgende vilkår:
-
-a.
-Det er mindre enn 5 år siden bryggen ble skadet/ødelagt og fungerte etter sin hensikt.
-
-b.
-Tiltaket medfører ikke utvidelse av opprinnelig godkjent brygge`,
-      spesialmerknad: `Nye brygger:
-
-Hensikten med bestemmelsen er å sette strenge rammer for etablering av regulerte brygger. Kravene i bestemmelsen går foran reguleringsplaners bestemmelser om brygge hvis det er motstrid. Alle vilkårene i a. til g. må være oppfylt.
-
-Eksiterende brygger:
-
-Hensikten med bestemmelsen er å sette strenge rammer for tiltak på eksisterende brygger. Tiltak på eksisterende brygge vil i utgangspunktet være omfattet av byggeforbudet etter 6.2, men kommunen har vurdert det som hensiktsmessig å unnta tiltak som oppfyller alle kravene i bestemmelsen fra byggeforbudet.
-
-“Lovlig etablert” betyr at bryggen har byggetillatelse/byggemelding, eller det kan dokumenteres at bryggen er oppført på et tidspunkt hvor det ikke var krav om byggetillatelse/byggemelding.
-
-På eksisterende lovlig etablerte brygger tillates ordinært vedlikehold, utskifting av materialer, mindre reparasjoner og nødvendige konstruktive forsterkninger, jf. pbl. § 1-8 tredje ledd, forutsatt at tiltaket ikke medfører utvidelse av bryggens areal, høyde eller ytterligere privatiserende virkning. Det tillates ikke etablering av nye brygger eller vesentlige endringer av eksisterende brygger uten reguleringsplan, jf. pbl. §§ 1-8 og 11-11 nr. 4.`,
+Alléer, verdifulle trær og vegetasjon skal bevares.
+      `,
+      spesialmerknad: `
+Hensikten med bestemmelsen er å sikre nødvendig etablering av
+kabler og ledningsnett.
+      `,
     },
   ],
 } as const;
